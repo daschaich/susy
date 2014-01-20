ce08c4eb0439d75255b80f76f1dbd1f65a5f1a53
David Schaich <dschaich@syr.edu>
2014-01-20 11:34:55 -0500

    Checkpoint serial pivoting algorithm that reproduces the C++ results (but about 3x faster).

--- 2dSYM/susy/d_phase.c
+++ 2dSYM/susy/d_phase.c
@@ -1,28 +1,7 @@
 // -----------------------------------------------------------------
 // Measure the phase of the pfaffian
-// Based on the algorithm in the appendix of hep-lat/0305002
-// hep-lat/9903014 provides important definitions and caveats (!!!)
 #include "susy_includes.h"
 #define PFA_TOL 1e-8
-// -----------------------------------------------------------------
-
-
-
-// -----------------------------------------------------------------
-// Return the inner product of two complex vectors, (a b)
-double_complex dot(complex *a, complex *b) {
-  int i, Ndat = 4 * DIMF;
-  complex tc;
-  double_complex dot = cmplx(0.0, 0.0);
-
-  for (i = 0; i < sites_on_node * Ndat; i++) {
-    CMUL(a[i], b[i], tc);   // !!! Note no conjugation!
-    CSUM(dot, tc);
-  }
-  // Accumulate inner product across all nodes
-  g_dcomplexsum(&dot);
-  return dot;
-}
 // -----------------------------------------------------------------
 
 
@@ -36,16 +15,15 @@ void matvec(complex *in, complex *out) {
 
   // Copy complex vector into Twist_Fermion src
   // Each Twist_Fermion has Ndat = 4DIMF non-trivial complex components
-  // !!! Need adjacent site/link/plaq fermions for functioning basis!
   FORALLSITES(i, s) {
-    clear_TF(&(src[i]));    // Clear diagonal plaquette fermions
     iter = i * Ndat;
     for (j = 0; j < DIMF; j++) {
       set_complex_equal(&(in[iter]), &(src[i].Fsite.c[j]));
       iter++;
-      for (mu = 0; mu < NUMLINK; mu++) {   // Do all links before any plaqs
+      for (mu = 0; mu < NUMLINK; mu++) {
         set_complex_equal(&(in[iter]), &(src[i].Flink[mu].c[j]));
         iter++;
+        src[i].Fplaq[mu][mu].c[j] = cmplx(0.0, 0.0);  // Clear diagonal Fplaq
         for (nu = mu + 1; nu < NUMLINK; nu++) {
           set_complex_equal(&(in[iter]), &(src[i].Fplaq[mu][nu].c[j]));
           CNEGATE(src[i].Fplaq[mu][nu].c[j], src[i].Fplaq[nu][mu].c[j]);
@@ -55,22 +33,10 @@ void matvec(complex *in, complex *out) {
     }
   }
 
-#ifdef DEBUG_CHECK
-  // Check that in and src have the same magnitude
-  complex tc = dot(in, in);
-  printf(" in magsq = %.4g\n", tc.real);
-
-  Real tr = 0.0;
-  FORALLSITES(i, s)
-    tr += magsq_TF(&(src[i]));
-  printf("src magsq = %.4g\n", tr);
-#endif
-
   fermion_op(src, res, 1);    // D
 
   // Copy the resulting Twist_Fermion res back to complex vector y
   // Each Twist_Fermion has Ndat = 4DIMF non-trivial complex components
-  // !!! Need adjacent site/link/plaq fermions for functioning basis!
   FORALLSITES(i, s) {
     iter = i * Ndat;
     for (j = 0; j < DIMF; j++) {
@@ -86,17 +52,6 @@ void matvec(complex *in, complex *out) {
       }
     }
   }
-
-#ifdef DEBUG_CHECK
-  // Check that in and src have the same magnitude
-  tr = 0.0;
-  FORALLSITES(i, s)
-    tr += magsq_TF(&(res[i]));
-  printf(" res magsq = %.4g\n", tr);
-
-  tc = dot(out, out);
-  printf(" out magsq = %.4g\n", tc.real);
-#endif
 }
 // -----------------------------------------------------------------
 
@@ -104,127 +59,183 @@ void matvec(complex *in, complex *out) {
 
 // -----------------------------------------------------------------
 void d_phase() {
-  register int i, j, k;
-  int Ndat = 4 * DIMF;
-  double phase, log_mag, tr;
-  complex temp, temp2;
-  complex *MonC = malloc(sites_on_node * Ndat * sizeof(*MonC));
-  complex **Q = malloc(sites_on_node * Ndat * sizeof(complex*));
+  register int i, j, k, pivot;
+  int Ndat = 4 * DIMF, interchange = 1;
+  double phase, log_mag, tr, maxSq;
+  complex tc, scale;
+  complex *col = malloc(volume * Ndat * sizeof(*col));
+  complex **D = malloc(volume * Ndat * sizeof(complex*));
 
-  // Allocate and initialize Q to unit matrix
+  // To be safe/explicit
+  for (i = 0; i < volume * Ndat; i++)
+    col[i] = cmplx(0.0, 0.0);
+
+  // Allocate and construct fermion operator D
   // Each Twist_Fermion has Ndat = 4DIMF non-trivial complex components
-  for (i = 0; i < sites_on_node * Ndat; i++) {
-    Q[i] = malloc(sites_on_node * Ndat * sizeof(complex));
-    for (j = 0; j < sites_on_node * Ndat; j++)
-      Q[i][j] = cmplx(0.0, 0.0);
-    Q[i][i] = cmplx(1.0, 0.0);
+  for (i = 0; i < volume * Ndat; i++) {
+    D[i] = malloc(volume * Ndat * sizeof(complex));
+    col[i] = cmplx(1.0, 0.0);
+    matvec(col, D[i]);
+    col[i] = cmplx(0.0, 0.0);
   }
+  free(col);    // Done with this
 
-  // Cycle over pairs of columns
-  for (i = 0; i < sites_on_node * Ndat - 1; i += 2) {
-    // e_{i + 1} --> e_{i + 1} / <e_{i + 1} | D | e_i>
-    // But only if <e_{i + 1} | D | e_i> is non-zero!
-    matvec(Q[i], MonC);
-#ifdef DEBUG_CHECK
-    // Check magnitude of MonC, compare with magnitude of res above
-    temp = dot(MonC, MonC);
-    printf("MonC magsq = %.4g\n", temp.real);
-#endif
-
-    temp = dot(Q[i + 1], MonC);
-    // !!! Must require non-vanishing matrix element!
-    if (cabs_sq(&temp) < PFA_TOL) {
-      printf("d_phase: <i+1 | D | i> = (%.4g, %.4g) too small for i = %d\n",
-             temp.real, temp.imag, i);
-      terminate(1);
-    }
-#ifdef DEBUG_CHECK
-    // Check complex number by which we're dividing
-    printf("(%.4g, %.4g)\n", temp.real, temp.imag);
-#endif
-    for (j = 0; j < sites_on_node * Ndat; j++) {
-      CDIV(Q[i + 1][j], temp, temp2);
-      set_complex_equal(&temp2, &(Q[i + 1][j]));
-    }
-
-    // Cycle over all subsequent columns
-    for (k = i + 2; k < sites_on_node * Ndat; k++) {
-      // e_k --> e_k - e_i <e_{i + 1} | D | e_k> - e_{i + 1} <e_i | D | e_k>
-      matvec(Q[k], MonC);
-#ifdef DEBUG_CHECK
-      // Check magnitude of MonC, compare with magnitude of res above
-      temp = dot(MonC, MonC);
-      printf("MonC magsq = %.4g\n", temp.real);
-#endif
-
-      temp = dot(Q[i + 1], MonC);
-      for (j = 0; j < sites_on_node * Ndat; j++) {
-        CMUL(Q[i][j], temp, temp2);
-        CDIF(Q[k][j], temp2);
-      }
-
-      temp = dot(Q[i], MonC);
-      for (j = 0; j < sites_on_node * Ndat; j++) {
-        CMUL(Q[i + 1][j], temp, temp2);
-        CSUM(Q[k][j], temp2);
+//#ifdef DEBUG_CHECK
+  // Check anti-symmetry of D
+  int count = 0;
+  for (i = 0; i < volume * Ndat; i++) {
+    for (j = i + 1; j < volume * Ndat; j++) {
+      if (D[i][j].real + D[j][i].real > PFA_TOL
+       || D[i][j].imag + D[j][i].imag > PFA_TOL) {
+        printf("d_phase: D[%d][%d] = (%.4g, %.4g) but ",
+               i, j, D[i][j].real, D[i][j].imag);
+        printf("D[%d][%d] = (%.4g, %.4g)\n",
+               j, i, D[j][i].real, D[j][i].imag);
       }
     }
+
+    // Make sure every column of D is non-vanishing
+    tr = 0.0;
+    for (j = 0; j < volume * Ndat; j++)
+      tr += cabs_sq(&(D[i][j]));
+    if (tr < PFA_TOL)
+      printf("d_phase: Column %d vanishes: %.4g\n", i, tr);
+
+    // Count number of non-zero elements in upper triangle
+    for (j = i; j < volume * Ndat; j++) {
+      if (cabs_sq(&(D[i][j])) > PFA_TOL)
+        count++;
+    }
+  }
+  printf("%d of %d elements of the full fermion matrix are non-zero\n",
+         2 * count, volume * Ndat * volume * Ndat);
+//#endif
+
+  // Now that we have an explicit matrix, loop over all pairs of its columns
+  for (i = 0; i < volume * Ndat - 1; i += 2) {
+    // Find row of the largest element in column i, to use as pivot
+    pivot = i + 1;
+    maxSq = cabs_sq(&(D[i][pivot]));
+    for (j = i + 2; j < volume * Ndat; j++) {
+      tr = cabs_sq(&(D[i][j]));
+      if (tr > maxSq) {
+        maxSq = tr;
+        pivot = j;
+      }
+    }
+//    if (maxSq < PFA_TOL) {    // We have a problem
+//      printf("d_phase: maxSq = %.4g for i = %d:\n", maxSq, i);
+//      for (j = 0; j < volume * Ndat; j++)
+//        printf("         (%.4g, %.4g)\n", D[i][j].real, D[i][j].imag);
+//      terminate(1);
+//    }
+
+    // If necessary, interchange row (i + 1) with pivot row
+    // Then interchange column (i + 1) with pivot column
+    if (pivot != i + 1) {
+      interchange *= -1;
+
+      for (j = 0; j < volume * Ndat; j++) {
+        set_complex_equal(&(D[j][i + 1]), &tc);
+        set_complex_equal(&(D[j][pivot]), &(D[j][i + 1]));
+        set_complex_equal(&tc, &(D[j][pivot]));
+      }
+      for (j = 0; j < volume * Ndat; j++) {
+        set_complex_equal(&(D[i + 1][j]), &tc);
+        set_complex_equal(&(D[pivot][j]), &(D[i + 1][j]));
+        set_complex_equal(&tc, &(D[pivot][j]));
+      }
+    }
+
+    // Now that maximum elements are in D[i][i + 1] and D[i + 1][i],
+    // progressively zero out elements of columns D[i] & D[i + 1]
+    // Then zero out elements of rows D[:][i] & D[:][i + 1]
+    for (j = i + 2; j < volume * Ndat; j++) {
+      // scale = D[i][j] / D[i][i + 1]
+      if (cabs_sq(&(D[i][i + 1])) < PFA_TOL) {
+        printf("d_phase: can't divide by D[%d][%d] = (%.4g, %.4g)\n",
+               i, i + 1, D[i][i + 1].real, D[i][i + 1].imag);
+        terminate(1);
+      }
+      CDIV(D[i][j], D[i][i + 1], scale);
+      for (k = 0; k < volume * Ndat; k++) {
+        CMUL(scale, D[k][i + 1], tc);
+        CDIF(D[k][j], tc);    // D[k][j] -= scale * D[k][i + 1]
+
+        CMUL(scale, D[i + 1][k], tc);
+        CDIF(D[j][k], tc);    // D[j][k] -= scale * D[i + 1][k]
+      }
+    }
+    for (j = i + 2; j < volume * Ndat; j++) {
+      // scale = D[i + 1][j] / D[i + 1][i]
+      if (cabs_sq(&(D[i + 1][i])) < PFA_TOL) {
+        printf("d_phase: can't divide by D[%d][%d] = (%.4g, %.4g)\n",
+               i + 1, i, D[i + 1][i].real, D[i + 1][i].imag);
+        terminate(1);
+      }
+      CDIV(D[i + 1][j], D[i + 1][i], scale);
+      for (k = 0; k < volume * Ndat; k++) {
+        CMUL(scale, D[k][i], tc);
+        CDIF(D[k][j], tc);    // D[k][j] -= scale * D[k][i]
+
+        CMUL(scale, D[i][k], tc);
+        CDIF(D[j][k], tc);    // D[j][k] -= scale * D[i][k]
+      }
+    }
+
+#ifdef DEBUG_CHECK
+    // Make sure every column of D is still non-vanishing
+    for (k = 0; k < volume * Ndat; k++) {
+      tr = 0.0;
+      for (j = k + 1; j < volume * Ndat; j++)
+        tr += cabs_sq(&(D[k][j]));
+      if (tr < PFA_TOL) {
+        printf("Column %d vanishes after round %d: %.4g\n", k, i, tr);
+        break;
+      }
+    }
+#endif
   }
 
 //#ifdef DEBUG_CHECK
-  // Checks on Q from hep-lat/0305002:
-  // <e_j | D | e_i> = 0 except
-  // <e_{i + 1} | D | e_i> = 1 for i even
-  // <e_{i - 1} | D | e_i> = -1 for i odd
-  complex one = cmplx(1.0, 0.0);
-  for (i = 0; i < sites_on_node * Ndat; i++) {
-    matvec(Q[i], MonC);
-    for (j = 0; j < sites_on_node * Ndat; j++) {
-      temp = dot(Q[j], MonC);
-      if (j == i + 1 && i % 2 == 0) {   // Braces suppress compiler warning
-        CDIF(temp, one);    // Should vanish
-      }
-      else if (j == i - 1 && i % 2 == 1)
-        CSUM(temp, one);    // Should vanish
-
-      if (temp.real > PFA_TOL || temp.imag > PFA_TOL) {
-        temp = dot(Q[j], MonC);
-        printf("PROBLEM: <%d | D | %d> = (%.4g, %.4g)\n",
-               j, i, temp.real, temp.imag);
+  // Check that D is still anti-symmetric, and now tridiagonal
+  for (i = 0; i < volume * Ndat; i++) {
+    for (j = i + 1; j < volume * Ndat; j++) {
+      if (D[i][j].real + D[j][i].real > PFA_TOL
+       || D[i][j].imag + D[j][i].imag > PFA_TOL) {
+        printf("d_phase: D[%d][%d] = (%.4g, %.4g) but ",
+               i, j, D[i][j].real, D[i][j].imag);
+        printf("D[%d][%d] = (%.4g, %.4g)\n",
+               j, i, D[j][i].real, D[j][i].imag);
       }
     }
   }
 //#endif
 
-  // Q is triangular by construction
-  // Compute its determinant from its diagonal elements
-  // expressed as the phase and (the log of) the magnitude
+  // Now we can just compute the pfaffian from D[i][i + 1],
+  // accumulating the phase and (the log of) the magnitude
   phase = 0.0;
   log_mag = 0.0;
-  for (i = 0; i < sites_on_node * Ndat; i++) {
-#ifdef DEBUG_CHECK
-    // Check: print out all diagonal elements
-    printf("%d: (%.4g, %.4g) --> exp[%.4g + %.4gi]\n",
-           i, Q[i][i].real, Q[i][i].imag,
-           log(cabs_sq(&(Q[i][i]))) / 2.0, carg(&(Q[i][i])));
-#endif
-
-    phase += carg(&(Q[i][i]));
-    log_mag += log(cabs_sq(&(Q[i][i]))) / 2.0;
-    free(Q[i]);
+  for (i = 0; i < volume * Ndat; i += 2) {
+//    printf("(%.4g, %.4g)\n", D[i][i + 1].real, D[i][i + 1].imag);
+    phase += carg(&(D[i][i + 1]));
+    log_mag += log(cabs_sq(&(D[i][i + 1]))) / 2.0;
+    free(D[i]);
   }
-  free(Q);
-  free(MonC);
+  free(D);
 
   // Accumulate phase and (log of) magnitude across all nodes
   g_doublesum(&phase);
   g_doublesum(&log_mag);
 
-  // pf(M) = (det Q)^{-1}
-  // Negate log of magnitude and phase, modding out 2pi
-  tr = -1.0 * phase;
+  // Account for potential negative sign from interchanges, and mod out 2pi
+  if (interchange > 1)
+    tr = phase;
+  else
+    tr = phase + PI;
   phase = fmod(tr, 2.0 * PI);
-  node0_printf("PFAFF %.8g %.8g %.8g %.8g\n", -1.0 * log_mag, phase,
+
+  node0_printf("PFAFF %.8g %.8g %.8g %.8g\n", log_mag, phase,
                fabs(cos(phase)), fabs(sin(phase)));
 }
 // -----------------------------------------------------------------
