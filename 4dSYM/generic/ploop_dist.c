57b7e28e8a55ca84354f5d1502e5f1343f9e9e88
daschaich <dschaich@syr.edu>
2014-09-18 12:58:50 -0400

    Remove broken PLOOPDIST option

--- 4dSYM/generic/ploop_dist.c
+++ 4dSYM/generic/ploop_dist.c
@@ -1,8 +1,8 @@
 // -----------------------------------------------------------------
 // Evaluate the Polyakov loops using general_gathers
 // Compute the Polyakov loop "at" the even sites in the first two time slices
-// If PLOOPDIST dist defined (currently broken!),
-// report the distribution of ploop values as a function of each x y z
+// For now we don't print the distribution of ploop values
+// as a function of each x y z, despite the file name
 #include "generic_includes.h"
 
 complex ploop() {
@@ -11,11 +11,6 @@ complex ploop() {
   int d[4] = {0, 0, 0, 0};    // Path from which to gather
   complex sum  = cmplx(0.0, 0.0), plp;
   msg_tag *tag;
-#ifdef PLOOPDIST
-  int x, y, z;
-  node0_printf("PLOOPDIST currently broken!");
-  terminate(1);
-#endif
 
   // First multiply the link on every even site by the link above it
   tag = start_gather_site(F_OFFSET(linkf[TUP]), sizeof(su3_matrix_f),
@@ -48,33 +43,8 @@ complex ploop() {
     if (s->t > 1)
       continue;
     plp = trace_su3_f(&(s->tempmat1));
-#ifdef PLOOPDIST
-    // Save result in tempmat1 for t = 0 or 1 slice
-    THIS IS WRONG
-    s->tempmat1.e[0][0] = plp;
-#endif
     CSUM(sum, plp);
   }
-
-#ifdef PLOOPDIST
-  // Report ploop distribution
-  for (x = 0; x < nx; x++) {
-    for (y = 0; y < ny; y++) {
-      for (z = 0; z < nz; z++) {
-        t = (x + y + z) % 2;
-        i = node_index(x, y, z, t);
-        if (node_number(x, y, z, t) != mynode())
-          plp = cmplx(0.0, 0.0);
-        THIS IS WRONG
-        else
-          plp = lattice[i].tempmat1.e[0][0];
-        g_sync();
-        g_complexsum(&plp);
-        node0_printf("PLOOP %d %d %d %.4g %.4g\n", x, y, z, plp.real, plp.imag);
-      }
-    }
-  }
-#endif
 
   g_complexsum(&sum);
   plp.real = sum.real / ((Real)(nx * ny * nz));
