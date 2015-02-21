72008524249ab90d7cc97ebbf9928b80e1868339
daschaich <daschaich@gmail.com>
2015-02-21 11:12:50 -0500

    Switching bilinear computation to U(1) noise sources

--- 4dSYM/susy/d_bilinear.c
+++ 4dSYM/susy/d_bilinear.c
@@ -13,6 +13,7 @@
 void bilinsrc(Twist_Fermion *g_rand, Twist_Fermion *src, int N) {
   register int i, j, mu;
   register site *s;
+  Real tr;
 
 #if 0
   // Testing...
@@ -42,25 +43,19 @@ void bilinsrc(Twist_Fermion *g_rand, Twist_Fermion *src, int N) {
     // The last Lambda[DIMF - 1] (N = DIMF) is proportional to the identity
     // The others (N = DIMF - 1) are traceless
     for (j = 0; j < N; j++) {                   // Site fermions
-#ifdef SITERAND
-      g_rand[i].Fsite.c[j].real = gaussian_rand_no(&(s->site_prn));
-      g_rand[i].Fsite.c[j].imag = gaussian_rand_no(&(s->site_prn));
-#else
-      g_rand[i].Fsite.c[j].real = gaussian_rand_no(&node_prn);
-      g_rand[i].Fsite.c[j].imag = gaussian_rand_no(&node_prn);
-#endif
+      tr = myrand(&node_prn) * TWOPI;
+      g_rand[i].Fsite.c[j] = cmplx(cos(tr), sin(tr));
     }
 
-    // Source all link fermions
+    // Source all link and plaquette fermions
     for (j = 0; j < DIMF; j++) {
       for (mu = XUP; mu < NUMLINK; mu++) {      // Link fermions
-#ifdef SITERAND
-        g_rand[i].Flink[mu].c[j].real = gaussian_rand_no(&(s->site_prn));
-        g_rand[i].Flink[mu].c[j].imag = gaussian_rand_no(&(s->site_prn));
-#else
-        g_rand[i].Flink[mu].c[j].real = gaussian_rand_no(&node_prn);
-        g_rand[i].Flink[mu].c[j].imag = gaussian_rand_no(&node_prn);
-#endif
+        tr = myrand(&node_prn) * TWOPI;
+        g_rand[i].Flink[mu].c[j] = cmplx(cos(tr), sin(tr));
+      }
+      for (mu = XUP; mu < NPLAQ; mu++) {        // Plaquette fermions
+        tr = myrand(&node_prn) * TWOPI;
+        g_rand[i].Fplaq[mu].c[j] = cmplx(cos(tr), sin(tr));
       }
     }
   }
@@ -193,7 +188,7 @@ int d_susyTrans() {
     return 0;
   register int i;
   register site *s;
-  int mu, j, isrc, iters, tot_iters = 0;
+  int mu, isrc, iters, tot_iters = 0;
   Real size_r, norm;
   double sum = 0.0;
   double_complex tc, StoL, LtoS, ave = cmplx(0.0, 0.0);
@@ -205,12 +200,10 @@ int d_susyTrans() {
   src = malloc(sites_on_node * sizeof(*src));
 
   // Hack a basic CG out of the multi-mass CG
-  setup_rhmc();
+  Norder = 1;
   psim = malloc(sizeof(**psim));
-  for (i = 0; i < Norder; i++) {
-    psim[i] = malloc(sites_on_node * sizeof(Twist_Fermion));
-    shift[i] = shift4[i];
-  }
+  psim[0] = malloc(sites_on_node * sizeof(Twist_Fermion));
+  shift[0] = 0;
 
   // Normalization: sum over NUMLINK but divide by volume
   // and divide by 2kappa as discussed on 15--17 December 2013
@@ -221,15 +214,7 @@ int d_susyTrans() {
     // Hit it with Mdag to get src, invert to get M^{-1} g_rand
     // congrad_multi_field initializes psim and calls fermion_rep()
     bilinsrc(g_rand, src, DIMF);
-    iters = 0;
-    for (mu = 0; mu < 4; mu++) {
-      iters += congrad_multi_field(src, psim, niter, rsqmin, &size_r);
-      FORALLSITES(i, s) {
-        scalar_mult_TF(&(src[i]), ampdeg4, &(src[i]));
-        for (j = 0; j < Norder; j++)
-          scalar_mult_add_TF(&(src[i]), &(psim[j][i]), amp4[j], &(src[i]));
-      } // We end with the full solution in src!
-    }
+    iters = congrad_multi_field(src, psim, niter, rsqmin, &size_r);
     tot_iters += iters;
 #ifdef DEBUG_CHECK
     dump_TF(&(psim[0][10]));    // Check what components are non-zero
@@ -241,7 +226,7 @@ int d_susyTrans() {
     LtoS = cmplx(0.0, 0.0);
     FORALLSITES(i, s) {
       for (mu = XUP; mu < NUMLINK; mu++) {
-        mult_su3_vec_adj_mat(&(src[i].Flink[mu]), &(s->link[mu]), &tvec);
+        mult_su3_vec_adj_mat(&(psim[0][i].Flink[mu]), &(s->link[mu]), &tvec);
         tc = su3_dot(&(g_rand[i].Fsite), &tvec);
 #ifdef DEBUG_CHECK
         printf("StoL[%d](%d) %d (%.4g, %.4g)\n",
@@ -249,7 +234,7 @@ int d_susyTrans() {
 #endif
         CSUM(StoL, tc);
 
-        mult_adj_su3_mat_vec(&(s->link[mu]), &(src[i].Fsite), &tvec);
+        mult_adj_su3_mat_vec(&(s->link[mu]), &(psim[0][i].Fsite), &tvec);
         tc = su3_dot(&(g_rand[i].Flink[mu]), &tvec);
 #ifdef DEBUG_CHECK
         printf("LtoS[%d](%d) %d (%.4g, %.4g)\n",
@@ -320,8 +305,8 @@ int d_susyTrans() {
     mult_su3_nn_f(&(DmuUmu[i]), &tmat, &tmat2);
     tc = trace_su3_f(&tmat2);
     // Make sure trace really is real
-    if (abs(tc.imag) > IMAG_TOL) {
-      printf("node%d WARNING: sum[%d]) = %.4g > %.4g)\n",
+    if (fabs(tc.imag) > IMAG_TOL) {
+      printf("node%d WARNING: Im(sum[%d]) = %.4g > %.4g\n",
              this_node, i, tc.imag, IMAG_TOL);
     }
     sum += tc.real;
