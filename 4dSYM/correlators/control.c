7a9bdd0260e505912201902a8c1074f24997b398
daschaich <daschaich@gmail.com>
2016-06-12 10:32:16 -0400

    Block correlators rather than correlating blocked operators

--- 4dSYM/correlators/control.c
+++ 4dSYM/correlators/control.c
@@ -10,9 +10,9 @@
 int main(int argc, char *argv[]) {
   register int i;
   register site *s;
-  int prompt, j, block, meas;
+  int prompt, j, meas;
   int y_dist, z_dist, t_dist;
-  Real one_ov_block, blockNorm, tr;
+  Real one_ov_meas, tr;
   double ss_plaq, st_plaq, dtime, vevK[N_K], vevS[N_K];
 #ifdef SMEAR
   double max_plaq = 0.0;
@@ -56,115 +56,96 @@ int main(int argc, char *argv[]) {
   // four-vectors per scalar distance are recorded in the norm array
   total_r = count_points(MAX_r);
 
-  // We can also set one_ov_block and blockNorm
-  // now that we have read in Nblock and Nmeas
-  one_ov_block = 1.0 / (Real)Nblock;
-  blockNorm = 1.0 / (Real)Nmeas;
-
-  // Loop over blocks, number of which is read in
-  for (block = 0; block < Nblock; block++) {
-    // Initialize operators for this block
-    FORALLSITES(i, s) {
-      for (j = 0; j < N_K; j++) {
-        ops[block][i].OK[j] = 0.0;
-        ops[block][i].OS[j] = 0.0;
-      }
-    }
-
-    // Loop over measurements per block, also read in
-    for (meas = 0; meas < Nmeas; meas++) {
-      j = block * Nmeas + meas;
-      startlat_p = reload_lattice(RELOAD_SERIAL, cfg[j]);
-
-      // Check: compute initial plaquette
-      plaquette(&ss_plaq, &st_plaq);
-      node0_printf("START %.8g %.8g %.8g\n", ss_plaq, st_plaq, ss_plaq + st_plaq);
-
-#ifdef SMEAR
-      // Optionally smear
-      if (smearflag == STOUT_SMEAR) {
-        node0_printf("Doing %d stout smearing steps ", Nsmear);
-        node0_printf("with rho=%.4g\n", alpha);
-      }
-      else if (smearflag == APE_SMEAR) {
-        node0_printf("Doing %d polar-projected APE smearing steps ", Nsmear);
-        node0_printf("with alpha=%.4g\n", alpha);
-      }
-
-      // Check minimum plaquette in addition to averages
-      node0_printf("BEFORE ");
-      max_plaq = local_plaquette(&ss_plaq, &st_plaq);   // Prints out MIN_PLAQ
-      node0_printf(" %.8g %.8g %.8g\n", ss_plaq, st_plaq, max_plaq);
-
-      // Overwrite s->link
-      if (smearflag == STOUT_SMEAR)
-        stout_smear(Nsmear, alpha);
-      else if (smearflag == APE_SMEAR)
-        APE_smear(Nsmear, alpha, YESDET);
-      node0_printf("AFTER  ");
-      max_plaq = local_plaquette(&ss_plaq, &st_plaq);      // Prints out MIN_PLAQ
-      node0_printf(" %.8g %.8g %.8g\n", ss_plaq, st_plaq, max_plaq);
-#endif
-
-      // Accumulate Konishi and SUGRA operators within this block
-      add_konishi(ops[block]);
-      node0_printf("\n");
-      fflush(stdout);
+  // Initialize site-by-site ensemble averages
+  FORALLSITES(i, s) {
+    for (j = 0; j < N_K; j++) {
+      tempops[i].OK[j] = 0.0;
+      tempops[i].OS[j] = 0.0;
     }
   }
 
-  // Now we can compute the vev at each site
-  // Use tempops to hold vev
-  // This is also a convenient place to normalize ops by Nmeas per block
-  // Might as well monitor volume average of average vacuum subtractions
+  // We can also set one_ov_meas now that we have read in Nblock and Nmeas
+  one_ov_meas = 1.0 / (Real)tot_meas;
+
+  // Loop over measurements, saving Konishi and SUGRA operators
+  // while accumulating site-by-site ensemble averages
+  for (meas = 0; meas < tot_meas; meas++) {
+    startlat_p = reload_lattice(RELOAD_SERIAL, cfg[meas]);
+
+    // Check: compute initial plaquette
+    plaquette(&ss_plaq, &st_plaq);
+    node0_printf("START %.8g %.8g %.8g\n", ss_plaq, st_plaq, ss_plaq + st_plaq);
+
+#ifdef SMEAR
+    // Optionally smear
+    if (smearflag == STOUT_SMEAR) {
+      node0_printf("Doing %d stout smearing steps ", Nsmear);
+      node0_printf("with rho=%.4g\n", alpha);
+    }
+    else if (smearflag == APE_SMEAR) {
+      node0_printf("Doing %d polar-projected APE smearing steps ", Nsmear);
+      node0_printf("with alpha=%.4g\n", alpha);
+    }
+
+    // Check minimum plaquette in addition to averages
+    node0_printf("BEFORE ");
+    max_plaq = local_plaquette(&ss_plaq, &st_plaq);   // Prints out MIN_PLAQ
+    node0_printf(" %.8g %.8g %.8g\n", ss_plaq, st_plaq, max_plaq);
+
+    // Overwrite s->link
+    if (smearflag == STOUT_SMEAR)
+      stout_smear(Nsmear, alpha);
+    else if (smearflag == APE_SMEAR)
+      APE_smear(Nsmear, alpha, YESDET);
+    node0_printf("AFTER  ");
+    max_plaq = local_plaquette(&ss_plaq, &st_plaq);      // Prints out MIN_PLAQ
+    node0_printf(" %.8g %.8g %.8g\n", ss_plaq, st_plaq, max_plaq);
+#endif
+
+    // Compute Konishi and SUGRA operators
+    konishi(ops[meas]);
+
+    // Accumulate site-by-site vev in tempops
+    FORALLSITES(i, s) {
+      for (j = 0; j < N_K; j++) {
+        tempops[i].OK[j] += ops[meas][i].OK[j];
+        tempops[i].OS[j] += ops[meas][i].OS[j];
+      }
+    }
+
+    node0_printf("\n");
+    fflush(stdout);
+  }
+
+  // Normalize site-by-site vevs and print volume averages
   for (j = 0; j < N_K; j++) {
     vevK[j] = 0.0;
     vevS[j] = 0.0;
   }
   FORALLSITES(i, s) {
-    // Initialization of running sums
     for (j = 0; j < N_K; j++) {
-      ops[0][i].OK[j] *= blockNorm;
-      ops[0][i].OS[j] *= blockNorm;
-
-      tempops[i].OK[j] = ops[0][i].OK[j];
-      tempops[i].OS[j] = ops[0][i].OS[j];
-    }
-    for (block = 1; block < Nblock; block++) {
-      for (j = 0; j < N_K; j++) {
-        ops[block][i].OK[j] *= blockNorm;
-        ops[block][i].OS[j] *= blockNorm;
-
-        tempops[i].OK[j] += ops[block][i].OK[j];
-        tempops[i].OS[j] += ops[block][i].OS[j];
-      }
-    }
-    // Normalize vev by number of blocks
-    for (j = 0; j < N_K; j++) {
-      tempops[i].OK[j] *= one_ov_block;
-      tempops[i].OS[j] *= one_ov_block;
+      tempops[i].OK[j] *= one_ov_meas;
+      tempops[i].OS[j] *= one_ov_meas;
 
       vevK[j] += tempops[i].OK[j];
       vevS[j] += tempops[i].OS[j];
     }
 
-    // Finally subtract vev from each block
-    for (block = 0; block < Nblock; block++) {
+    // Finally subtract vevs from each measurement
+    for (meas = 0; meas < tot_meas; meas++) {
       for (j = 0; j < N_K; j++) {
-        ops[block][i].OK[j] -= tempops[i].OK[j];
-        ops[block][i].OS[j] -= tempops[i].OS[j];
+        ops[meas][i].OK[j] -= tempops[i].OK[j];
+        ops[meas][i].OS[j] -= tempops[i].OS[j];
       }
     }
   }
-
-  // Monitor volume average of average vacuum subtractions
   for (j = 0; j < N_K; j++)
     node0_printf("VEV_K %d %.8g\n", j, vevK[j] / (Real)volume);
   for (j = 0; j < N_K; j++)
     node0_printf("VEV_S %d %.8g\n", j, vevS[j] / (Real)volume);
 
   // Now analyze correlators of vacuum-subtracted operators,
-  // including jackknife uncertainties
+  // including blocked standard error
   correlator_r();
 
   node0_printf("RUNNING COMPLETED\n");
