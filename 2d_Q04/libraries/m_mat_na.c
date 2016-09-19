7c484127b9ce8c50f0b33a9d09893edc6cd6d814
daschaich <daschaich@gmail.com>
2016-09-19 15:57:10 +0200

    Fix U(N) adjoint rep and remove -DFAST option, which slowed down 4d HMC test

--- 2d_Q04/libraries/m_mat_na.c
+++ 2d_Q04/libraries/m_mat_na.c
@@ -35,7 +35,6 @@ void mult_na_dif(matrix *a, matrix *b, matrix *c) {
   }
 }
 
-#ifndef FAST
 void mult_na(matrix *a, matrix *b, matrix *c) {
   register int i, j, k;
   for (i = 0; i < NCOL; i++) {
@@ -54,53 +53,4 @@ void mult_na(matrix *a, matrix *b, matrix *c) {
     }
   }
 }
-#else   // FAST version for NCOL=3 only
-void mult_na(matrix *a, matrix *b, matrix *c) {
-  int i,j;
-  register Real t, ar, ai, br, bi, cr, ci;
-  for (i = 0; i < 3; i++) {
-    for (j = 0; j < 3; j++) {
-      ar = a->e[i][0].real;
-      ai = a->e[i][0].imag;
-      br = b->e[j][0].real;
-      bi = b->e[j][0].imag;
-      cr = ar * br;
-      t = ai * bi;
-      cr += t;
-      ci = ai * br;
-      t = ar * bi;
-      ci -= t;
-
-      ar = a->e[i][1].real;
-      ai = a->e[i][1].imag;
-      br = b->e[j][1].real;
-      bi = b->e[j][1].imag;
-      t = ar * br;
-      cr += t;
-      t = ai * bi;
-      cr += t;
-      t = ar * bi;
-      ci -= t;
-      t = ai * br;
-      ci += t;
-
-      ar = a->e[i][2].real;
-      ai = a->e[i][2].imag;
-      br = b->e[j][2].real;
-      bi = b->e[j][2].imag;
-      t = ar * br;
-      cr += t;
-      t = ai * bi;
-      cr += t;
-      t = ar * bi;
-      ci -= t;
-      t = ai * br;
-      ci += t;
-
-      c->e[i][j].real = cr;
-      c->e[i][j].imag = ci;
-    }
-  }
-}
-#endif
 // -----------------------------------------------------------------
