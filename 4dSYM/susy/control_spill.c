ce4e932c6f8498d8ab4315458390b0b3945db12c
daschaich <daschaich@gmail.com>
2017-01-12 12:15:46 +0100

    A little cleanup

--- 4dSYM/susy/control_spill.c
+++ 4dSYM/susy/control_spill.c
@@ -6,7 +6,6 @@
 int main(int argc, char *argv[]) {
   int prompt, s, x, y, z, t, mu, i, j;
   Real re, im;
-  double dssplaq, dstplaq;
 
   // Setup
   setlinebuf(stdout); // DEBUG
@@ -35,6 +34,7 @@ int main(int argc, char *argv[]) {
 
   // Spill lattice in format expected by serial code
   // (cf. read_in.cpp, loop_over_lattice and << overloading for Umatrix
+  // Also need to add first line: nx\t nt\t kappa\t f_eps\t N
   for (t = 0; t < nt; t++) {
     for (z = 0; z < nz; z++) {
       for (y = 0; y < ny; y++) {
