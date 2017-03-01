b4b8eb4ccf69afcdfe430e79cb3761672153ad77
rgjha <rgjha1989@gmail.com>
2017-03-01 17:45:34 -0500

    Fixing some small typos

--- 4dSYM/susy/setup.c
+++ 4dSYM/susy/setup.c
@@ -22,7 +22,7 @@ int initial_set() {
     printf("Microcanonical simulation with refreshing\n");
     printf("Machine = %s, with %d nodes\n", machine_type(), numnodes());
 #ifdef HYBRID
-    printf("Gauge links in SL(N,C) \n");
+    printf("Gauge links in SL(N,C), running in hybrid mode \n");
 #endif
 #ifdef HMC_ALGORITHM
     printf("Hybrid Monte Carlo algorithm\n");
