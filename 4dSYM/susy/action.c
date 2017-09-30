daschaich/susy

commit f23226b868a8e00766383d6cf821b3b3fe4a4729

A little bit of cleanup

4dSYM/susy/action.c
@@ -182,7 +182,7 @@ double gauge_action(int do_det) {
 double bmass_action() {
   register int i, a;
   register site *s;
-  double sum = 0.0, kappa_muSq = kappa * bmass * bmass;
+  double sum = 0.0;
 #ifdef EIG_POT
   matrix tmat;
 #else
@@ -197,10 +197,11 @@ double bmass_action() {
       sum += kappa_muSq * realtrace(&tmat, &tmat);
 #else
       tr = one_ov_N * realtrace(&(s->link[a]), &(s->link[a])) - 1.0;
-      sum += kappa_muSq * tr * tr;
+      sum += tr * tr;
 #endif
     }
   }
+  sum *= kappa * bmass * bmass;
   g_doublesum(&sum);
   return sum;
 }

4dSYM/generic/io_lat.c
@@ -29,7 +29,7 @@
    configuration for lattice files and for propagator files, the
    propagator for a single source spin-color combination.  Data in these
    files appear as a series of 32-bit floating point numbers.  We treat
-   the 32-bit values as unsigned integers v(i) where i = 0,...,N-1 ranges
+   the 32-bit values as unsigned integers v(i) where i = 0, ..., N-1 ranges
    over the values in the order of appearance on the file and N is the
    total count of values in the data set.  The checksum is obtained by a
    combination of bit-rotations and exclusive or operations.  It is
@@ -133,7 +133,7 @@ gauge_file *w_serial_i(char *filename) {
 
 
 // -----------------------------------------------------------------
-// Flush lbuf to output, resetting buf_length is reset
+// Flush lbuf to output, resetting buf_length
 static void flush_lbuf_to_file(gauge_file *gf, fmatrix *lbuf,
                                int *buf_length) {
 
@@ -233,9 +233,8 @@ void w_serial(gauge_file *gf) {
   off_t checksum_offset = 0;  // Location of checksum
   off_t gauge_check_size;     // Size of checksum record
 
-
-  // Allocate message buffer space for one x dimension of the local hypercube
-  // The largest possible space we need is nx
+  // tbuf holds message buffer space for the x dimension
+  // of the local hypercube (needs at most nx * NUMLINK matrices)
   if (tbuf == NULL) {
     printf("w_serial: node%d can't malloc tbuf\n", this_node);
     terminate(1);
@@ -278,10 +277,9 @@ void w_serial(gauge_file *gf) {
   gf->check.sum29 = 0;
   // Count 32-bit words mod 29 and mod 31 in order of appearance on file
   // Here only node 0 uses these values -- both start at 0
-  rank29 = NUMLINK * sizeof(fmatrix)
-                   / sizeof(int32type) * sites_on_node * this_node % 29;
-  rank31 = NUMLINK * sizeof(fmatrix)
-                   / sizeof(int32type) * sites_on_node * this_node % 31;
+  i = sizeof(fmatrix) / sizeof(int32type) * sites_on_node * this_node;
+  rank29 = (NUMLINK * i) % 29;
+  rank31 = (NUMLINK * i) % 31;
 
   g_sync();
   currentnode = 0;  // The node delivering data
@@ -291,7 +289,8 @@ void w_serial(gauge_file *gf) {
     for (z = 0; z < nz; z++) {
       for (y = 0; y < ny; y++) {
         for (x = 0; x < nx; x++, j++) {
-          newnode = node_number(x, y, z, t);  // The node providing the next site
+          // The node providing the next site
+          newnode = node_number(x, y, z, t);
           if (newnode != currentnode || x == 0) {
             // We are switching to a new node or have exhausted a line of nx
             // Sweep any data in the retiring node's tbuf to the node0 lbuf
@@ -372,18 +371,17 @@ void w_serial(gauge_file *gf) {
 // -----------------------------------------------------------------
 // Only node 0 reads the gauge configuration gf from a binary file
 void r_serial(gauge_file *gf) {
-  FILE *fp;
-  gauge_header *gh;
-  char *filename;
-  int byterevflag;
+  FILE *fp = gf->fp;
+  gauge_header *gh = gf->header;
+  char *filename = gf->filename;
+  int byterevflag = gf->byterevflag;
 
   off_t offset = 0;           // File stream pointer
   off_t gauge_check_size;     // Size of gauge configuration checksum record
   off_t coord_list_size;      // Size of coordinate list in bytes
   off_t head_size;            // Size of header plus coordinate list
   off_t checksum_offset = 0;  // Where we put the checksum
-  int rcv_rank, rcv_coords;
-  int destnode, stat;
+  int rcv_rank, rcv_coords, destnode, stat, idest = 0;
   int k, x, y, z, t;
   int buf_length = 0, where_in_buf = 0;
   gauge_check test_gc;
@@ -391,11 +389,6 @@ void r_serial(gauge_file *gf) {
   int rank29, rank31;
   fmatrix *lbuf = NULL;   // Only allocate on node0
   fmatrix tmat[NUMLINK];
-  int idest = 0;
-  fp = gf->fp;
-  gh = gf->header;
-  filename = gf->filename;
-  byterevflag = gf->byterevflag;
 
   if (this_node == 0) {
     // Compute offset for reading gauge configuration
@@ -411,8 +404,8 @@ void r_serial(gauge_file *gf) {
     checksum_offset = gf->header->header_bytes + coord_list_size;
     head_size = checksum_offset + gauge_check_size;
 
-    // Allocate single precision read buffer
-    lbuf = malloc(MAX_BUF_LENGTH * NUMLINK * sizeof(fmatrix));
+    // Allocate single-precision read buffer
+    lbuf = malloc(MAX_BUF_LENGTH * NUMLINK * sizeof(*lbuf));
     if (lbuf == NULL) {
       printf("r_serial: node%d can't malloc lbuf\n", this_node);
       fflush(stdout);
@@ -442,11 +435,10 @@ void r_serial(gauge_file *gf) {
 
   g_sync();
 
-  /* node0 reads and deals out the values */
+  // node0 reads and deals out the values
   for (rcv_rank = 0; rcv_rank < volume; rcv_rank++) {
     /* If file is in coordinate natural order, receiving coordinate
        is given by rank. Otherwise, it is found in the table */
-
     if (gf->header->order == NATURAL_ORDER)
       rcv_coords = rcv_rank;
     else
@@ -463,16 +455,16 @@ void r_serial(gauge_file *gf) {
     // The node that gets the next set of gauge links
     destnode = node_number(x, y, z, t);
 
+    // node0 fills its buffer, if necessary
     if (this_node == 0) {
-      /* node0 fills its buffer, if necessary */
       if (where_in_buf == buf_length) {  /* get new buffer */
         /* new buffer length  = remaining sites, but never bigger
            than MAX_BUF_LENGTH */
         buf_length = volume - rcv_rank;
         if (buf_length > MAX_BUF_LENGTH)
           buf_length = MAX_BUF_LENGTH;
-        /* then do read */
 
+        // Now do read
         stat = (int)fread(lbuf, NUMLINK * sizeof(fmatrix), buf_length, fp);
         if (stat != buf_length) {
           printf("r_serial: node%d gauge configuration read error %d file %s\n",

4dSYM/libraries/make_ahmat.c
@@ -1,5 +1,6 @@
 // -----------------------------------------------------------------
-// Take and compress the traceless anti-hermitian part of a matrix
+// Compute and compress the traceless anti-hermitian part of a matrix
+// dest = 0.5 * (src - src^dag) - Tr[0.5 * (src - src^dag)]
 #include "../include/config.h"
 #include "../include/complex.h"
 #include "../include/susy.h"
@@ -18,8 +19,8 @@ void make_anti_hermitian(matrix *src, anti_hermitmat *dest) {
 
   for (i = 0; i < NCOL; i++) {
     for (j = i + 1; j < NCOL; j++) {
-      dest->m[index].real = (src->e[i][j].real - src->e[j][i].real) * 0.5;
-      dest->m[index].imag = (src->e[i][j].imag + src->e[j][i].imag) * 0.5;
+      dest->m[index].real = 0.5 * (src->e[i][j].real - src->e[j][i].real);
+      dest->m[index].imag = 0.5 * (src->e[i][j].imag + src->e[j][i].imag);
       index++;
     }
   }

4dSYM/susy/update_h.c
@@ -133,7 +133,7 @@ double gauge_force(Real eps) {
     }
   }
 
-  // Third we have the scalar potential derivative contribution
+  // Third we have the Q-exact scalar potential derivative contribution
   //   [...] = Udag_mu(x) 2B^2/N Tr[DmuUmu](x) Y(x)
   // where Y(x) = Tr[U_mu(x) Udag_mu(x)] / N - 1
   // Only compute if B is non-zero
@@ -256,7 +256,8 @@ double gauge_force(Real eps) {
       scalar_mult_matrix(&(s->f_U[mu]), kappa, &(s->f_U[mu]));
   }
 
-  // Only compute U(1) mass term if non-zero -- note factor of kappa
+  // Only compute susy-breaking scalar potential term if bmass non-zero
+  // Note factor of kappa
   if (bmass > IMAG_TOL) {
     Real dmu;
 #ifdef EIG_POT

commit 23985f10e5416ed97ce4f437880bae3ff13e6796

Fix eig_pot error introduced in f23226b

4dSYM/susy/action.c
@@ -194,7 +194,7 @@ double bmass_action() {
 #ifdef EIG_POT
       mult_na(&(s->link[a]), &(s->link[a]), &tmat);
       scalar_add_diag(&tmat, -1.0);
-      sum += kappa_muSq * realtrace(&tmat, &tmat);
+      sum += realtrace(&tmat, &tmat);
 #else
       tr = one_ov_N * realtrace(&(s->link[a]), &(s->link[a])) - 1.0;
       sum += tr * tr;
@@ -205,6 +205,42 @@ double bmass_action() {
   g_doublesum(&sum);
   return sum;
 }
+// -----------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------
+// Center-breaking term that protects the single-link 'Wilson line'
+// in reduced direction(s)
+// Comes with factors of kappa and -2
+#ifdef DIMREDUCE
+double cWline_action() {
+  register int i, a;
+  register site *s;
+  double sum = 0.0;
+#ifdef EIG_POT
+  matrix tmat;
+#else
+  Real tr;
+#endif
+
+  FORALLUPDIR(a) {
+    FORALLSITES(i, s) {
+#ifdef EIG_POT
+      mult_na(&(s->link[a]), &(s->link[a]), &tmat);
+      scalar_add_diag(&tmat, -1.0);
+      sum += realtrace(&tmat, &tmat);
+#else
+      tr = one_ov_N * realtrace(&(s->link[a]), &(s->link[a])) - 1.0;
+      sum += tr * tr;
+#endif
+    }
+  }
+  sum *= -2.0 * kappa * cWline * cWline;
+  g_doublesum(&sum);
+  return sum;
+}
+#endif
 // -----------------------------------------------------------------
 
 
@@ -295,7 +331,7 @@ double mom_action() {
 // Print out zeros for pieces of the action that aren't included
 double action(Twist_Fermion **src, Twist_Fermion ***sol) {
   double g_act, bmass_act = 0.0, p_act, f_act, det_act = 0.0;
-  double total;
+  double cWline_act = 0.0, total;
 
   g_act = gauge_action(NODET);
   if (bmass > IMAG_TOL)
@@ -307,7 +343,13 @@ double action(Twist_Fermion **src, Twist_Fermion ***sol) {
   node0_printf("gauge %.8g bmass %.8g ", g_act, bmass_act);
   node0_printf("det %.8g ", det_act);
 
-  total = g_act + bmass_act + det_act;
+#ifdef DIMREDUCE
+  if (cWline > IMAG_TOL)
+    cWline_act = cWline_action();
+  node0_printf("cWline %.8g ", cWline_act);
+#endif
+
+  total = g_act + bmass_act + det_act + cWline_act;
 #ifndef PUREGAUGE
   int n;
   for (n = 0; n < Nroot; n++) {