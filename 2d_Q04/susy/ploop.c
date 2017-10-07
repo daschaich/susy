daschaich/susy

commit 27efc0e3b277693b3f8dc66da7bf17c9c6ec17d6

Last of the 2d cleanup for now

2d_Q04/susy/ploop.c
@@ -26,11 +26,11 @@ complex ploop(int dir, int project, double *plpMod) {
   switch(dir) {
     case XUP:
       len = nx;
-      norm = (double)(nt);
+      norm = 1.0 / (double)(nt);
       break;
     case TUP:
       len = nt;
-      norm = (double)(nx);
+      norm = 1.0 / (double)(nx);
       break;
     default:
       printf("ploop: unrecognized direction %d\n", dir);
@@ -95,8 +95,8 @@ complex ploop(int dir, int project, double *plpMod) {
   }
   g_complexsum(&sum);
   g_doublesum(plpMod);
-  CDIVREAL(sum, norm, sum);
-  *plpMod /= norm;
+  CMULREAL(sum, norm, sum);
+  *plpMod *= norm;
 
   if (project == 1) {       // Reset original links
     FORALLSITES(i, s)

4d_Q16/susy/ploop.c
@@ -26,23 +26,23 @@ complex ploop(int dir, int project, double *plpMod) {
   switch(dir) {
     case XUP:
       len = nx;
-      norm = (double)(nt * ny * nz);
+      norm = 1.0 / (double)(nt * ny * nz);
       break;
     case YUP:
       len = ny;
-      norm = (double)(nx * nt * nz);
+      norm = 1.0 / (double)(nx * nt * nz);
       break;
     case ZUP:
       len = nz;
-      norm = (double)(nx * ny * nt);
+      norm = 1.0 / (double)(nx * ny * nt);
       break;
     case TUP:
       len = nt;
-      norm = (double)(nx * ny * nz);
+      norm = 1.0 / (double)(nx * ny * nz);
       break;
     case DIR_5:
       len = nt;
-      norm = (double)(nx * ny * nz);
+      norm = 1.0 / (double)(nx * ny * nz);
       break;
     default:
       printf("ploop: unrecognized direction %d\n", dir);
@@ -115,8 +115,8 @@ complex ploop(int dir, int project, double *plpMod) {
   }
   g_complexsum(&sum);
   g_doublesum(plpMod);
-  CDIVREAL(sum, norm, sum);
-  *plpMod /= norm;
+  CMULREAL(sum, norm, sum);
+  *plpMod *= norm;
 
   if (project == 1) {       // Reset original links
     FORALLSITES(i, s)

2d_Q04/susy/README
@@ -20,6 +20,7 @@ Eigenvalue measurements (susy_eig) require PRIMME (https://github.com/primme/pri
 # Defines for compilation:
 -DPHI_ALGORITHM for the basic phi algorithm (REQUIRED)
 -DHMC_ALGORITHM switches on the accept/reject step
+-DEIG_POT switches from a double-trace to a single-trace scalar potential (not actively tested)
 -DCORR switches on the Konishi and SUGRA correlator calculation
 -DSMEAR switches on optional smearing for measurements
 -DWLOOP switches on the gauge-fixed Wilson loop calculation
@@ -108,7 +109,7 @@ setup_lambda.c        -- Set up U(N) generators, epsilon tensor and lookup table
 setup_offset.c        -- Set up gathers for terms in the action, especially Qclosed terms
 action.c              -- Compute the action, including related routines also needed by forces
 plaq.c                -- Compute the average plaquette
-ploop.c               -- Compute the average Polyakov loop
+ploop.c               -- Compute the average Polyakov loop or Wilson line
 link_trace.c          -- Compute average link, Tr[U_a Ubar_a] / NCOL
 widths.c              -- Calculate sqrt(<P^2> - <P>^2) for the plaquette and plaquette determinant
 determinant.c         -- Utilities for finding the determinant of NCOL x NCOL link matrices, using LAPACK
@@ -144,6 +145,7 @@ hvy_pot_polar_loop.c -- Compute more standard on-axis Wilson loops of polar-proj
                         (NB: overwrites links, as above)
 path.c               -- Used to construct Wilson loops for hvy_pot_loop.c and hvy_pot_polar_loop.c
 rsymm.c              -- Compute modified Wilson loops related to R symmetry transformations
+scalar_eig.c         -- Compute eigenvalues of scalar fields
 
 # 1d) Additional files used only by MCRG target (susy_mcrg)
 control_mcrg.c       -- Main program for RG blocking and blocked measurements
@@ -168,6 +170,7 @@ phase.c         -- Phase and (log of) magnitude of the pfaffian
 # 1g) Files not currently used, retained for potential future testing
 ploop_corr.c      -- Find Polyakov loop on every spatial site to be fed into FFT
 update_leapfrog.c -- Leapfrog RHMC evolution
+phase_serial.c    -- A serial gaussian elimination pfaffian algorithm, to check the parallel computation
 
 # 2) Summary of files in the ../generic directory:
 # 2a) Files used by all executables

2d_Q04/susy/action.c
@@ -198,35 +198,6 @@ double bmass_action() {
 
 
 // -----------------------------------------------------------------
-// Center-breaking term that protects the single-link 'Wilson line'
-// in reduced direction(s)
-// Comes with factors of kappa and -2
-#ifdef DIMREDUCE
-double cWline_action() {
-  register int i, mu;
-  register site *s;
-  double sum = 0.0;
-  matrix tmat;
-
-  FORALLUPDIR(mu) {
-    if (length[mu] == 1) {
-      FORALLSITES(i, s) {
-        invert(&(s->link[mu]), &tmat);
-        sum_matrix(&(s->link[mu]), &tmat);    // U + U^(-1)
-        sum += trace(&tmat).real;
-      }
-    }
-  }
-  sum *= -2.0 * kappa * cWline * cWline;
-  g_doublesum(&sum);
-  return sum;
-}
-#endif
-// -----------------------------------------------------------------
-
-
-
-// -----------------------------------------------------------------
 // Plaquette determinant contribution to the action
 // Assume compute_plaqdet() has already been run
 double det_action() {
@@ -323,12 +294,6 @@ double action(Twist_Fermion **src, Twist_Fermion ***sol) {
   node0_printf("action: ");
   node0_printf("gauge %.8g bmass %.8g ", g_act, bmass_act);
   node0_printf("det %.8g ", det_act);
-
-#ifdef DIMREDUCE
-  if (cWline > IMAG_TOL)
-    cWline_act = cWline_action();
-  node0_printf("cWline %.8g ", cWline_act);
-#endif
 
   total = g_act + bmass_act + det_act + cWline_act;
 #ifndef PUREGAUGE

2d_Q04/susy/block_mcrg.c
@@ -1,36 +1,1 @@
-// -----------------------------------------------------------------
-// Blocking function for MCRG
-// Just multiplies together "neighboring" links separated by 2^block sites
-// Saves all 2^d products in sub-lattices of original gauge configuration
-// Overwrite links, use site->mom for temporary storage
-#include "susy_includes.h"
-
-void block_mcrg(int block) {
-  register int dir, i;
-  register site *s;
-  int disp[NDIMS], j, bl = 1;
-  msg_tag *tag;
-
-  // Set number of links to stride, bl = 2^(block - 1)
-  for (j = 1; j < block; j++)
-    bl *= 2;
-  FORALLDIR(dir) {
-    for (j = 0; j < NDIMS; j++)
-      disp[j] = bl * offset[dir][j];
-
-    tag = start_general_gather_site(F_OFFSET(link[dir]), sizeof(matrix),
-                                    disp, EVENANDODD, gen_pt[0]);
-    wait_general_gather(tag);
-
-    FORALLSITES(i, s)
-      mult_nn(&(s->link[dir]), (matrix *)(gen_pt[0][i]), &(s->mom[dir]));
-    cleanup_general_gather(tag);
-  }
-
-  // Overwrite original links
-  FORALLSITES(i, s) {
-    FORALLDIR(dir)
-      mat_copy(&(s->mom[dir]), &(s->link[dir]));
-  }
-}
-// -----------------------------------------------------------------
+../../4d_Q16/susy/block_mcrg.c

2d_Q04/susy/blocked_ops.c
@@ -1,53 +1,1 @@
-// -----------------------------------------------------------------
-// Konishi and SUGRA operators integrated over lattice volume
-#include "susy_includes.h"
-
-void blocked_ops(int Nsmear, int block) {
-  register int i;
-  register site *s;
-  int a, b, j, bl = 2;
-  double norm, OK[N_K], OS[N_K];
-
-  // Initialize Konishi and SUGRA operators
-  for (j = 0; j < N_K; j++) {
-    OK[j] = 0.0;
-    OS[j] = 0.0;
-  }
-
-  // Compute traces of bilinears of scalar field interpolating ops
-  compute_Ba();
-
-  // Now sum operators over lattice volume
-  FORALLSITES(i, s) {
-    FORALLDIR(a) {
-      for (j = 0; j < N_K; j++) {
-        OK[j] += traceBB[j][a][a][i];   // Konishi
-
-        // Now SUGRA, averaged over 20 off-diagonal components
-        FORALLDIR(b) {
-          if (a == b)
-            continue;
-          OS[j] += 0.05 * traceBB[j][a][b][i];
-        }
-      }
-    }
-  }
-  for (j = 0; j < N_K; j++) {
-    g_doublesum(&(OK[j]));
-    g_doublesum(&(OS[j]));
-  }
-
-  // Print each operator summed over volume
-  // Have to divide by number of blocked lattices: 2^(4block) = bl^4
-  // Format: TAG  smearing  block  a  op[a]
-  for (j = 1; j < block; j++)
-    bl *= 2;
-  if (block <= 0)
-    bl = 1;
-  norm = (Real)(bl * bl * bl * bl);
-  for (j = 0; j < N_K; j++)    // Konishi
-    node0_printf("OK %d %d %d %.8g\n", Nsmear, block, j, OK[j] / norm);
-  for (j = 0; j < N_K; j++)    // SUGRA
-    node0_printf("OS %d %d %d %.8g\n", Nsmear, block, j, OS[j] / norm);
-}
-// -----------------------------------------------------------------
+../../4d_Q16/susy/blocked_ops.c

2d_Q04/susy/congrad_multi.c
@@ -1,240 +1,1 @@
-// -----------------------------------------------------------------
-// Multi-mass conjugate gradient algorithm a la B. Jegerlehner
-// The number of masses is runtime input Norder
-// shift[Norder] is the array of mass values, set up in setup_rhmc.c
-// BEWARE: The temporary TF pm[0] is never malloced or used
-
-// At least for now we hard-code a zero initial guess
-// We check all psi for convergence and quit doing the converged ones
-
-// In this version of the code, all the scalars are real because M = Ddag D
-//#define CG_DEBUG
-#include "susy_includes.h"
-// -----------------------------------------------------------------
-
-
-
-// -----------------------------------------------------------------
-// Keep all Norder-dependent mallocs here so that we can change Norder
-// Return number of iterations
-// src is where the source is created
-// psim[Norder] are working TFs for the conjugate gradient
-// MaxCG is the maximum number of iterations per solve
-// errormin is the target |r|^2, scaled below by source_norm = |src|^2
-// size_r is the final obtained |r|^2, hopefully < errormin * source_norm
-int congrad_multi(Twist_Fermion *src, Twist_Fermion **psim,
-                  int MaxCG, Real errormin, Real *size_r) {
-
-  register int i, j;
-  register site *s;
-  int N_iter, iteration = 0;
-  int *converged = malloc(Norder * sizeof(*converged));
-  Real floatvar, floatvar2;     // SSE kluge
-  Real *floatvarj = malloc(Norder * sizeof(*floatvarj));
-  Real *floatvark = malloc(Norder * sizeof(*floatvark));
-  double rsq, rsqnew, source_norm = 0.0, rsqstop, c1, c2, cd;
-  double *zeta_i   = malloc(Norder * sizeof(*zeta_i));
-  double *zeta_im1 = malloc(Norder * sizeof(*zeta_im1));
-  double *zeta_ip1 = malloc(Norder * sizeof(*zeta_ip1));
-  double *beta_i   = malloc(Norder * sizeof(*beta_i));
-  double *beta_im1 = malloc(Norder * sizeof(*beta_im1));
-  double *alpha    = malloc(Norder * sizeof(*alpha));
-  double rsqj;
-  complex ctmp;
-  Twist_Fermion **pm = malloc(Norder * sizeof(**pm));
-  for (i = 1; i < Norder; i++)    // !!!
-    pm[i] = malloc(sites_on_node * sizeof(Twist_Fermion));
-
-  // Initialize zero initial guess, etc.
-  // dest = 0, r = source, pm[j] = r
-  for (i = 0; i < Norder; i++)
-    converged[i] = 0;
-  FORALLSITES(i, s) {
-    copy_TF(&(src[i]), &(rm[i]));
-    copy_TF(&(rm[i]), &(pm0[i]));
-    clear_TF(&(psim[0][i]));
-    for (j = 1; j < Norder; j++) {
-      clear_TF(&(psim[j][i]));
-      copy_TF(&(rm[i]), &(pm[j][i]));
-    }
-  }
-
-  FORALLSITES(i, s)
-    source_norm += (double)magsq_TF(&(src[i]));
-  g_doublesum(&source_norm);
-  rsq = source_norm;
-  rsqstop = errormin * source_norm;
-#ifdef CG_DEBUG
-  node0_printf("congrad: source_norm = %.4g\n", source_norm);
-  node0_printf("stopping when residue is %.4g\n", rsqstop);
-#endif
-
-  for (j = 0; j < Norder; j++) {
-    zeta_im1[j] = 1;
-    zeta_i[j] = 1;
-    alpha[j] = 0;
-    beta_im1[j] = 1;
-  }
-
-  for (N_iter = 0; N_iter < MaxCG && rsq > rsqstop; N_iter++) {
-    // mp = (M(u) + shift[0]) pm
-    DSq(pm0, mpm);
-    iteration++;
-    total_iters++;
-    FORALLSITES(i, s)
-      scalar_mult_sum_TF(&(pm0[i]), shift[0], &(mpm[i]));
-
-    // beta_i[0] = -(r, r) / (pm, Mpm)
-    cd = 0;
-    FORALLSITES(i, s) {
-      ctmp = TF_dot(&(pm0[i]), &(mpm[i]));
-      cd += ctmp.real;
-    }
-    g_doublesum(&cd);
-
-    beta_i[0] = -rsq / cd;
-#ifdef CG_DEBUG
-    node0_printf("beta_i %.4g rsq %.4g cd %.4g\n", beta_i[0], rsq, cd);
-#endif
-
-    // beta_i(sigma)
-    // zeta_ip1(sigma)
-    zeta_ip1[0] = 1;
-    for (j = 1; j < Norder; j++) {
-      if (converged[j] == 0) {
-        zeta_ip1[j] = zeta_i[j] * zeta_im1[j] * beta_im1[0];
-        c1 = beta_i[0] * alpha[0] * (zeta_im1[j] - zeta_i[j]);
-        c2 = zeta_im1[j] * beta_im1[0] * (1 - (shift[j] - shift[0]) * beta_i[0]);
-        zeta_ip1[j] /= c1 + c2;
-        beta_i[j] = beta_i[0] * zeta_ip1[j] / zeta_i[j];
-      }
-    }
-
-    // psim[j] = psim[j] - beta[j] * pm[j]
-    floatvar = -(Real)beta_i[0];
-    for (j = 1; j < Norder; j++) {
-      if (converged[j] == 0)
-        floatvarj[j] = -(Real)beta_i[j];
-    }
-
-    FORALLSITES(i, s) {
-      scalar_mult_sum_TF(&(pm0[i]), floatvar, &(psim[0][i]));
-      for (j = 1; j < Norder; j++) {
-        if (converged[j] == 0)
-          scalar_mult_sum_TF(&(pm[j][i]), floatvarj[j], &(psim[j][i]));
-      }
-    }
-
-    // r = r + beta[0] * mp
-    floatvar = (Real)beta_i[0];
-    FORALLSITES(i, s)
-      scalar_mult_sum_TF(&(mpm[i]), floatvar, &(rm[i]));
-
-    // alpha_ip1[j]
-    rsqnew = 0;
-    FORALLSITES(i, s)
-      rsqnew += (double)magsq_TF(&(rm[i]));
-
-    g_doublesum(&rsqnew);
-    alpha[0] = rsqnew / rsq;
-#ifdef CG_DEBUG
-    node0_printf("alpha %.4g rsqnew %.4g rsq %.4g\n", alpha[0], rsqnew, rsq);
-#endif
-
-    // alpha_ip11 -- note shifted indices with respect to Eq. 2.43!
-    for (j = 1; j < Norder; j++) {
-      if (converged[j] == 0)
-        alpha[j] = alpha[0] * zeta_ip1[j] * beta_i[j] / (zeta_i[j] * beta_i[0]);
-    }
-
-    // pm[j] = zeta_ip1[j] * r + alpha[j] * pm[j]
-    floatvar  = (Real)zeta_ip1[0];
-    floatvar2 = (Real)alpha[0];
-    for (j = 1; j < Norder; j++) {
-      floatvarj[j] = (Real)zeta_ip1[j];
-      floatvark[j] = (Real)alpha[j];
-    }
-    FORALLSITES(i, s) {
-      scalar_mult_TF(&(rm[i]),floatvar, &(mpm[i]));
-      scalar_mult_add_TF(&(mpm[i]), &(pm0[i]), floatvar2, &(pm0[i]));
-      for (j = 1; j < Norder; j++) {
-        if (converged[j] == 0) {
-          scalar_mult_TF(&(rm[i]), floatvarj[j], &(mpm[i]));
-          scalar_mult_add_TF(&(mpm[i]), &(pm[j][i]), floatvark[j], &(pm[j][i]));
-        }
-      }
-    }
-
-    // Test for convergence
-    rsq = rsqnew;
-    for (j = 1; j < Norder; j++) {
-      if (converged[j] == 0) {
-        rsqj = rsq * zeta_ip1[j] * zeta_ip1[j];
-        if (rsqj <= rsqstop) {
-          converged[j] = 1;
-#ifdef CG_DEBUG
-          node0_printf(" psi%d converged in %d steps, rsq = %.4g\n",
-                       j, N_iter, rsqj);
-#endif
-        }
-      }
-    }
-#ifdef CG_DEBUG
-    if ((N_iter / 10) * 10 == N_iter) {
-      node0_printf("iter %d residue %.4g\n", N_iter, (double)(rsq));
-      fflush(stdout);
-    }
-#endif
-
-    // Scroll scalars
-    for (j = 0; j < Norder; j++) {
-      if (converged[j] == 0) {
-        beta_im1[j] = beta_i[j];
-        zeta_im1[j] = zeta_i[j];
-        zeta_i[j] = zeta_ip1[j];
-      }
-    }
-  }
-  if (rsq > rsqstop)
-    node0_printf(" multi CONGRAD not converged\n rsq = %.4g\n", rsq);
-
-  *size_r = rsq;
-
-  // Test inversion
-#ifdef CG_DEBUG
-  for (j = 0; j < Norder; j++) {
-    source_norm = 0;
-    FORALLSITES(i, s)
-      source_norm += (double)magsq_TF(&(psim[j][i]));
-
-    g_doublesum(&source_norm);
-    node0_printf("Norm of psim %d shift %.4g is %.4g\n",
-                 j, shift[j], source_norm);
-
-    DSq(psim[j], mpm);              // mpm = (D^2 + fmass^2).psim[j]
-    source_norm = 0;                // Re-using for convenience
-    FORALLSITES(i, s) {             // Add shift.psi and subtract src
-      scalar_mult_sum_TF(&(psim[j][i]), shift[j],  &(mpm[i]));
-      scalar_mult_sum_TF(&(src[i]), -1.0, &(mpm[i]));
-      source_norm += (double)magsq_TF(&(mpm[i]));
-    }
-    g_doublesum(&source_norm);
-    node0_printf("%d Test of (D^2 + shift)psi - src: %.4g\n", j, source_norm);
-  }
-#endif
-
-  for (i = 1; i < Norder; i++)
-    free(pm[i]);
-  free(pm);
-  free(zeta_i);
-  free(zeta_ip1);
-  free(zeta_im1);
-  free(beta_im1);
-  free(beta_i);
-  free(alpha);
-  free(converged);
-  free(floatvarj);
-  free(floatvark);
-  return iteration;
-}
-// -----------------------------------------------------------------
+../../4d_Q16/susy/congrad_multi.c

2d_Q04/susy/defines.h
@@ -28,7 +28,6 @@
                           // |det-1|^2 is not actively tested!!!
 #define SV                // Site/vector terms in action
 #define VP                // Vector/plaquette terms in action
-//#define DIMREDUCE         // Consider dimensionally reduced systems
 //#define DEBUG_CHECK       // Print lambdas, offsets, etc.
 
 // Tunable parameter in gauge action

commit 7636a7ff977b7a314bb826b3f36e27165cbafbbc

Fix minor normalization problem introduced by 27efc0e

2d_Q04/susy/ploop.c
@@ -48,8 +48,8 @@ complex ploop(int dir, int project, double *plpMod) {
     }
     g_complexsum(&sum);
     g_doublesum(plpMod);
-    CDIVREAL(sum, norm, sum);
-    *plpMod /= norm;
+    CMULREAL(sum, norm, sum);
+    *plpMod *= norm;
 
     if (project == 1) {       // Reset original links
       FORALLSITES(i, s)

4d_Q16/susy/ploop.c
@@ -60,8 +60,8 @@ complex ploop(int dir, int project, double *plpMod) {
     }
     g_complexsum(&sum);
     g_doublesum(plpMod);
-    CDIVREAL(sum, norm, sum);
-    *plpMod /= norm;
+    CMULREAL(sum, norm, sum);
+    *plpMod *= norm;
 
     if (project == 1) {       // Reset original links
       FORALLSITES(i, s)