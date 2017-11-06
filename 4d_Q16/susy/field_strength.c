703f649b3c597c00262b0da0a1d9f1055f0dcff3
daschaich <daschaich@gmail.com>
2017-11-06 10:57:10 +0100

    Cherry-picking old wflow work

---
+++ 4d_Q16/susy/field_strength.c
@@ -0,0 +1,150 @@
+// -----------------------------------------------------------------
+// Compute SU(3) field strength tensor
+// Need to define six field strength indices
+// Use tempmat and tempmat2 for temporary storage
+
+// This cartoon shows how the plaquettes are calculated
+// The path begins and ends at the 'O' at the corner
+// F_munu is the sum of these plaquettes minus their adjoints
+
+//  ^         --------<--------       --------<--------
+//  |dir2     |               |       |               |
+//  |         |               |       |               |
+//  |         |               |       |               |
+//  |         |               ^       |               ^
+//  ------>   |               |       |               |
+//    dir     |               |       |               |
+//            |               |       |               |
+//            |               |       |               |
+//            -------->-------O       O------->--------
+//
+//            --------<-------O       O-------<--------
+//            |               |       |               |
+//            |               |       |               |
+//            |               |       |               |
+//            |               ^       |               ^
+//            |               |       |               |
+//            |               |       |               |
+//            |               |       |               |
+//            |               |       |               |
+//            -------->--------       -------->--------
+
+// Convention: try to use gen_pt[0] and mtag for links in direction dir
+// These are gathered from +/- dir2
+
+#include "susy_includes.h"
+
+// link_src is offset for matrix link[4] in site struct
+// field_dest is offset for matrix fieldstrength[6] in site struct
+void make_field_strength() {
+  register int i, component, dir, dir2;
+  register site *s;
+  int j;
+  complex cc;
+  matrix tmat, tmat2;
+  msg_tag *mtag, *mtag2;
+
+  FORALLDIR(dir) {
+    for (dir2 = dir + 1; dir2 < NUMLINK; dir2++) {
+      component = plaq_index[dir][dir2];
+
+      // +dir +dir2 plaquette
+      mtag = start_gather_site(F_OFFSET(link[dir]), sizeof(matrix),
+                               goffset[dir2], EVENANDODD, gen_pt[0]);
+      mtag2 = start_gather_site(F_OFFSET(link[dir2]), sizeof(matrix),
+                                goffset[dir], EVENANDODD, gen_pt[1]);
+
+      wait_gather(mtag);
+      wait_gather(mtag2);
+      FORALLSITES(i, s) {
+        mult_nn(&s->link[dir], (matrix *)(gen_pt[1][i]), &tmat);
+        mult_na(&tmat, (matrix *)(gen_pt[0][i]), &tmat2);
+        mult_na(&tmat2, &s->link[dir2], &tmat);
+        adjoint(&tmat, &tmat2);
+        sub_matrix(&tmat, &tmat2, &FS[component][i]);
+      }
+      cleanup_gather(mtag2);
+
+      // -dir +dir2 plaquette
+      // Reuse link[dir] gather from dir2 corresponding to mtag
+      FORALLSITES(i, s) {
+        mult_an(&s->link[dir2], &s->link[dir], &tmat);
+        mult_an((matrix *)(gen_pt[0][i]), &tmat, &(tempmat[i]));
+      }
+      mtag2 = start_gather_field(tempmat, sizeof(matrix),
+                                 goffset[dir]+1, EVENANDODD, gen_pt[1]);
+
+      wait_gather(mtag2);
+      FORALLSITES(i, s) {
+        mult_nn(&s->link[dir2], (matrix *)(gen_pt[1][i]), &tmat);
+        adjoint(&tmat, &tmat2);
+        sum_matrix(&tmat, &FS[component][i]);
+        dif_matrix(&tmat2, &FS[component][i]);
+      }
+      cleanup_gather(mtag);
+      cleanup_gather(mtag2);
+
+      // -dir -dir2 plaquette
+      mtag = start_gather_site(F_OFFSET(link[dir]), sizeof(matrix),
+                               goffset[dir]+1, EVENANDODD, gen_pt[0]);
+      mtag2 = start_gather_site(F_OFFSET(link[dir2]), sizeof(matrix),
+                                goffset[dir2]+1, EVENANDODD, gen_pt[1]);
+
+      wait_gather(mtag);
+      wait_gather(mtag2);
+      FORALLSITES(i,s){
+        mult_nn((matrix *)(gen_pt[0][i]), &s->link[dir2], &(tempmat[i]));
+        mult_nn((matrix *)(gen_pt[1][i]), &s->link[dir], &(tempmat2[i]));
+      }
+      cleanup_gather(mtag);
+      cleanup_gather(mtag2);
+
+      mtag = start_gather_field(tempmat, sizeof(matrix),
+                                goffset[dir2]+1, EVENANDODD, gen_pt[0]);
+      mtag2 = start_gather_field(tempmat2, sizeof(matrix),
+                                 goffset[dir]+1, EVENANDODD, gen_pt[1]);
+
+      wait_gather(mtag);
+      wait_gather(mtag2);
+      FORALLSITES(i,s){
+        mult_an((matrix *)(gen_pt[1][i]),(matrix *)(gen_pt[0][i]), &tmat);
+        adjoint(&tmat, &tmat2);
+        sum_matrix(&tmat, &FS[component][i]);
+        dif_matrix(&tmat2, &FS[component][i]);
+      }
+      cleanup_gather(mtag);
+      cleanup_gather(mtag2);
+
+      // +dir -dir2 plaquette
+      mtag2 = start_gather_site(F_OFFSET(link[dir2]), sizeof(matrix),
+                                goffset[dir], EVENANDODD, gen_pt[1]);
+
+      wait_gather(mtag2);
+      FORALLSITES(i, s) {
+        mult_an(&s->link[dir2], &s->link[dir], &tmat);
+        mult_nn(&tmat, (matrix *)(gen_pt[1][i]), &tempmat[i]);
+      }
+      cleanup_gather(mtag2);
+
+      mtag = start_gather_field(tempmat, sizeof(matrix),
+                                goffset[dir2]+1, EVENANDODD, gen_pt[0]);
+      wait_gather(mtag);
+      FORALLSITES(i,s){
+        mult_na((matrix *)(gen_pt[0][i]), &s->link[dir], &tmat);
+        adjoint(&tmat, &tmat2);
+        sum_matrix(&tmat, &FS[component][i]);
+        dif_matrix(&tmat2, &FS[component][i]);
+      }
+      cleanup_gather(mtag);
+
+      // Make traceless
+      FORALLSITES(i, s) {
+        cc = trace(&FS[component][i]);
+        CMULREAL(cc, one_ov_N, cc);
+        for(j = 0; j < NCOL; j++)
+          CDIF(FS[component][i].e[j][j], cc);
+      }
+    }
+  }
+}
+// -----------------------------------------------------------------
---
+++ 4d_Q16/susy/wflow.c
@@ -0,0 +1,157 @@
+// -----------------------------------------------------------------
+// Run Wilson flow
+// Runge--Kutta coefficients computed from Eq. 2.4 of arXiv:1203.4469
+// !!! arXiv 1006.4518 provides useful details
+#include "susy_includes.h"
+// -----------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------
+// Clear A
+void clear_antiH(anti_hermitmat *a) {
+  int i;
+  for (i = 0; i < NCOL; i++)
+    a->im_diag[i] = 0.0;
+
+  for (i = 0; i < NCOL * (NCOL - 1) / 2; i++)
+    a->m[i] = cmplx(0.0, 0.0);
+}
+
+// c <-- c + s * b (output is always last)
+void scalar_mult_sum_antiH(anti_hermitmat *b, Real s, anti_hermitmat *c) {
+  int i;
+  for (i = 0; i < NCOL; i++)
+    c->im_diag[i] += s * b->im_diag[i];
+
+  for (i = 0; i < NCOL * (NCOL - 1) / 2; i++) {
+    c->m[i].real += s * b->m[i].real;
+    c->m[i].imag += s * b->m[i].imag;
+  }
+}
+// -----------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------
+// Sum staples for direction dir over all other directions
+void compute_staple() {
+  register int i;
+  register site *s;
+  int dir, dir2;
+
+  FORALLSITES(i, s)
+    FORALLDIR(dir) {
+      mat_copy(&(s->link[dir]), &(s->mom[dir]));
+  }
+
+  FORALLDIR(dir) {
+    FORALLSITES(i, s)
+      clear_mat(&(staple[i]));
+
+    FORALLDIR(dir2) {
+      if (dir == dir2)
+        continue;
+      directional_staple(dir, dir2);
+    }
+
+    FORALLSITES(i, s)
+      mat_copy(&(staple[i]), &(S[dir][i]));
+  }
+}
+// -----------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------
+// Calculate Q = Q + f1 * Project_antihermitian_traceless(U.Sdag)
+//           U = exp(f2 * Q).U
+// S is the Lie derivative of the action being used to flow
+void update_flow(Real f1, Real f2) {
+  register int i, dir;
+  register site *s;
+  matrix tmat;
+  anti_hermitmat tmat_ah;
+
+  // This is where we can change the 'flow action'
+  compute_staple();
+
+  FORALLDIR(dir) {
+    FORALLSITES(i, s) {
+      mult_na(&(s->link[dir]), &(S[dir][i]), &tmat);
+      make_anti_hermitian(&tmat, &tmat_ah);
+      // Q += f1 * U.S
+      scalar_mult_sum_antiH(&tmat_ah, f1, &(Q[dir][i]));
+    }
+  }
+  exp_mult(f2);             // U = exp(f2 * Q).U
+}
+// -----------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------
+void stout_step_rk() {
+  register int i, dir;
+  register site *s;
+
+  // Clear Q, just in case
+  FORALLSITES(i, s) {
+    FORALLDIR(dir)
+      clear_antiH(&(Q[dir][i]));
+  }
+
+  update_flow(17.0 * wflow_eps / 36.0, -9.0 / 17.0);
+  update_flow(-8.0 * wflow_eps / 9.0, 1.0);
+  update_flow(3.0 * wflow_eps / 4.0, -1.0);
+}
+// -----------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------
+void wflow() {
+  register int i, dir;
+  register site *s;
+  int istep;
+  double t = 0.0, E, tSqE, old_tSqE = 0.0, der_tSqE;
+  double ssplaq, stplaq, plaq, check;
+
+  // Go
+  for (istep = 0; fabs(t) <  fabs(tmax) - 0.5 * fabs(wflow_eps); istep++) {
+    stout_step_rk();
+    t += wflow_eps;
+
+    // Find 8F_munu = sum_{clover} (U - Udag)
+    // Subtract the (lattice artifact?) trace at each lattice site
+    make_field_strength();
+
+    // Compute t^2 E and its slope
+    E = 0.0;
+    FORALLSITES(i, s) {
+      for (dir = 0; dir < 10; dir++)    // TODO: Check minus sign, no FS dagger
+        E -= (double)realtrace_nn(&(FS[dir][i]), &(FS[dir][i]));
+    }
+    g_doublesum(&E);
+    E /= (volume * 64.0); // Normalization factor of 1/8 for each F_munu
+    tSqE = t * t * E;
+    der_tSqE = fabs(t) * (tSqE - old_tSqE) / fabs(wflow_eps);
+    // Any negative signs in t and wflow_eps should cancel out anyway...
+
+    // Check with plaquette
+    plaquette(&ssplaq, &stplaq);
+    plaq = 0.5 * (ssplaq + stplaq);
+    // TODO: Guessing numerical factor
+    check = 20.0 * t * t * fabs((double)NCOL - plaq);
+
+    // Monitor bosonic action along flow
+    compute_DmuUmu();
+    compute_Fmunu();
+    ssplaq = gauge_action(NODET) / ((double)volume * 4.5 * NCOL * NCOL);
+
+    old_tSqE = tSqE;
+    node0_printf("WFLOW %g %g %g %g %g %g %g\n",
+                 t, plaq, E, tSqE, der_tSqE, check, ssplaq);
+  }
+}
+// -----------------------------------------------------------------
