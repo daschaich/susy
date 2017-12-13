daschaich/susy

commit 27efc0e3b277693b3f8dc66da7bf17c9c6ec17d6

Last of the 2d cleanup for now

4d_Q16/susy/hvy_pot_loop.c
@@ -19,7 +19,7 @@ void hvy_pot_loop(int do_det) {
   // Use tempmat to hold loop product at each site
   for (t_dist = 1; t_dist <= MAX_T; t_dist++) {
     // Set up rectangular path as list of dir * sign
-    for (mu = XUP; mu <= ZUP; mu++) {
+    FORALLUPDIR(mu) {
       for (x_dist[mu] = 0; x_dist[mu] <= MAX_X; x_dist[mu]++) {
         if (mu > 0 && x_dist[mu] == 0)
           x_dist[mu] = 1;   // Only check (0, T) "loop" once
@@ -68,9 +68,9 @@ void hvy_pot_loop(int do_det) {
           node0_printf("PLOT_LOOP ");
         node0_printf("%d %d %d %d %.6g\n", x_dist[0], x_dist[1], x_dist[2],
                      t_dist, wloop / volume);
-      } // x_dist[mu]
+      }
       x_dist[mu] = 0;
-    } // mu
-  } // t_dist
+    }
+  }
 }
 // -----------------------------------------------------------------

4d_Q16/susy/hvy_pot_polar_loop.c
@@ -30,7 +30,7 @@ void hvy_pot_polar_loop() {
   // Use tempmat to hold loop product at each site
   for (t_dist = 1; t_dist <= MAX_T; t_dist++) {
     // Set up rectangular path as list of dir * sign
-    for (mu = XUP; mu <= ZUP; mu++) {
+    FORALLUPDIR(mu) {
       for (x_dist[mu] = 0; x_dist[mu] <= MAX_X; x_dist[mu]++) {
         if (mu > 0 && x_dist[mu] == 0)
           x_dist[mu] = 1;   // Only check (0, T) "loop" once
@@ -70,9 +70,9 @@ void hvy_pot_polar_loop() {
         node0_printf("PLOLAR_LOOP %d %d %d %d %.6g\n",
                      x_dist[0], x_dist[1], x_dist[2], t_dist,
                      polarloop / volume);
-      } // x_dist[mu]
+      }
       x_dist[mu] = 0;
-    } // mu
-  } // t_dist
+    }
+  }
 }
 // -----------------------------------------------------------------

2d_Q04/susy/hvy_pot_loop.c
@@ -1,7 +1,6 @@
 // -----------------------------------------------------------------
-// Wilson loops for fundamental links (hardwired in path)
-// This version calls path to compute simple rectangular loops
-// so gauge fixing has no effect
+// Wilson loops for static potential using path
+// This version computes simple rectangular loops so gauge fixing has no effect
 // Use tempmat for temporary storage
 #include "susy_includes.h"
 
@@ -63,7 +62,7 @@ void hvy_pot_loop(int do_det) {
       else
         node0_printf("PLOT_LOOP ");
       node0_printf("%d %d %.6g\n", x_dist, t_dist, wloop / volume);
-    } // x_dist
-  } // t_dist
+    }
+  }
 }
 // -----------------------------------------------------------------

2d_Q04/susy/hvy_pot_polar_loop.c
@@ -1,8 +1,7 @@
 // -----------------------------------------------------------------
-// Wilson loops for fundamental links (hardwired in path)
+// Wilson loops for polar-projected static potential using path
 // Links unitarized via polar projection -- overwritten!!!
-// This version calls path to compute simple rectangular loops
-// so gauge fixing has no effect
+// This version computes simple rectangular loops so gauge fixing has no effect
 // Use tempmat for temporary storage
 #include "susy_includes.h"
 
@@ -65,7 +64,7 @@ void hvy_pot_polar_loop() {
       g_doublesum(&polarloop);
       node0_printf("PLOLAR_LOOP %d %d %.6g\n",
                    x_dist, t_dist, polarloop / volume);
-    } // x_dist
-  } // t_dist
+    }
+  }
 }
 // -----------------------------------------------------------------

2d_Q04/susy/README
@@ -20,6 +20,7 @@ Eigenvalue measurements (susy_eig) require PRIMME (https://github.com/primme/pri
 # Defines for compilation:
 -DPHI_ALGORITHM for the basic phi algorithm (REQUIRED)
 -DHMC_ALGORITHM switches on the accept/reject step
+-DEIG_POT switches from a double-trace to a single-trace scalar potential (not actively tested)
 -DCORR switches on the Konishi and SUGRA correlator calculation
 -DSMEAR switches on optional smearing for measurements
 -DWLOOP switches on the gauge-fixed Wilson loop calculation
@@ -108,7 +109,7 @@ setup_lambda.c        -- Set up U(N) generators, epsilon tensor and lookup table
 setup_offset.c        -- Set up gathers for terms in the action, especially Qclosed terms
 action.c              -- Compute the action, including related routines also needed by forces
 plaq.c                -- Compute the average plaquette
-ploop.c               -- Compute the average Polyakov loop
+ploop.c               -- Compute the average Polyakov loop or Wilson line
 link_trace.c          -- Compute average link, Tr[U_a Ubar_a] / NCOL
 widths.c              -- Calculate sqrt(<P^2> - <P>^2) for the plaquette and plaquette determinant
 determinant.c         -- Utilities for finding the determinant of NCOL x NCOL link matrices, using LAPACK
@@ -144,6 +145,7 @@ hvy_pot_polar_loop.c -- Compute more standard on-axis Wilson loops of polar-proj
                         (NB: overwrites links, as above)
 path.c               -- Used to construct Wilson loops for hvy_pot_loop.c and hvy_pot_polar_loop.c
 rsymm.c              -- Compute modified Wilson loops related to R symmetry transformations
+scalar_eig.c         -- Compute eigenvalues of scalar fields
 
 # 1d) Additional files used only by MCRG target (susy_mcrg)
 control_mcrg.c       -- Main program for RG blocking and blocked measurements
@@ -168,6 +170,7 @@ phase.c         -- Phase and (log of) magnitude of the pfaffian
 # 1g) Files not currently used, retained for potential future testing
 ploop_corr.c      -- Find Polyakov loop on every spatial site to be fed into FFT
 update_leapfrog.c -- Leapfrog RHMC evolution
+phase_serial.c    -- A serial gaussian elimination pfaffian algorithm, to check the parallel computation
 
 # 2) Summary of files in the ../generic directory:
 # 2a) Files used by all executables

2d_Q04/susy/action.c
@@ -198,35 +198,6 @@ double bmass_action() {
 
 
 // -----------------------------------------------------------------
-// Center-breaking term that protects the single-link 'Wilson line'
-// in reduced direction(s)
-// Comes with factors of kappa and -2
-#ifdef DIMREDUCE
-double cWline_action() {
-  register int i, mu;
-  register site *s;
-  double sum = 0.0;
-  matrix tmat;
-
-  FORALLUPDIR(mu) {
-    if (length[mu] == 1) {
-      FORALLSITES(i, s) {
-        invert(&(s->link[mu]), &tmat);
-        sum_matrix(&(s->link[mu]), &tmat);    // U + U^(-1)
-        sum += trace(&tmat).real;
-      }
-    }
-  }
-  sum *= -2.0 * kappa * cWline * cWline;
-  g_doublesum(&sum);
-  return sum;
-}
-#endif
-// -----------------------------------------------------------------
-
-
-
-// -----------------------------------------------------------------
 // Plaquette determinant contribution to the action
 // Assume compute_plaqdet() has already been run
 double det_action() {
@@ -323,12 +294,6 @@ double action(Twist_Fermion **src, Twist_Fermion ***sol) {
   node0_printf("action: ");
   node0_printf("gauge %.8g bmass %.8g ", g_act, bmass_act);
   node0_printf("det %.8g ", det_act);
-
-#ifdef DIMREDUCE
-  if (cWline > IMAG_TOL)
-    cWline_act = cWline_action();
-  node0_printf("cWline %.8g ", cWline_act);
-#endif
 
   total = g_act + bmass_act + det_act + cWline_act;
 #ifndef PUREGAUGE

2d_Q04/susy/block_mcrg.c
@@ -1,36 +1,1 @@
-// -----------------------------------------------------------------
-// Blocking function for MCRG
-// Just multiplies together "neighboring" links separated by 2^block sites
-// Saves all 2^d products in sub-lattices of original gauge configuration
-// Overwrite links, use site->mom for temporary storage
-#include "susy_includes.h"
-
-void block_mcrg(int block) {
-  register int dir, i;
-  register site *s;
-  int disp[NDIMS], j, bl = 1;
-  msg_tag *tag;
-
-  // Set number of links to stride, bl = 2^(block - 1)
-  for (j = 1; j < block; j++)
-    bl *= 2;
-  FORALLDIR(dir) {
-    for (j = 0; j < NDIMS; j++)
-      disp[j] = bl * offset[dir][j];
-
-    tag = start_general_gather_site(F_OFFSET(link[dir]), sizeof(matrix),
-                                    disp, EVENANDODD, gen_pt[0]);
-    wait_general_gather(tag);
-
-    FORALLSITES(i, s)
-      mult_nn(&(s->link[dir]), (matrix *)(gen_pt[0][i]), &(s->mom[dir]));
-    cleanup_general_gather(tag);
-  }
-
-  // Overwrite original links
-  FORALLSITES(i, s) {
-    FORALLDIR(dir)
-      mat_copy(&(s->mom[dir]), &(s->link[dir]));
-  }
-}
-// -----------------------------------------------------------------
+../../4d_Q16/susy/block_mcrg.c

2d_Q04/susy/blocked_ops.c
@@ -1,53 +1,1 @@
-// -----------------------------------------------------------------
-// Konishi and SUGRA operators integrated over lattice volume
-#include "susy_includes.h"
-
-void blocked_ops(int Nsmear, int block) {
-  register int i;
-  register site *s;
-  int a, b, j, bl = 2;
-  double norm, OK[N_K], OS[N_K];
-
-  // Initialize Konishi and SUGRA operators
-  for (j = 0; j < N_K; j++) {
-    OK[j] = 0.0;
-    OS[j] = 0.0;
-  }
-
-  // Compute traces of bilinears of scalar field interpolating ops
-  compute_Ba();
-
-  // Now sum operators over lattice volume
-  FORALLSITES(i, s) {
-    FORALLDIR(a) {
-      for (j = 0; j < N_K; j++) {
-        OK[j] += traceBB[j][a][a][i];   // Konishi
-
-        // Now SUGRA, averaged over 20 off-diagonal components
-        FORALLDIR(b) {
-          if (a == b)
-            continue;
-          OS[j] += 0.05 * traceBB[j][a][b][i];
-        }
-      }
-    }
-  }
-  for (j = 0; j < N_K; j++) {
-    g_doublesum(&(OK[j]));
-    g_doublesum(&(OS[j]));
-  }
-
-  // Print each operator summed over volume
-  // Have to divide by number of blocked lattices: 2^(4block) = bl^4
-  // Format: TAG  smearing  block  a  op[a]
-  for (j = 1; j < block; j++)
-    bl *= 2;
-  if (block <= 0)
-    bl = 1;
-  norm = (Real)(bl * bl * bl * bl);
-  for (j = 0; j < N_K; j++)    // Konishi
-    node0_printf("OK %d %d %d %.8g\n", Nsmear, block, j, OK[j] / norm);
-  for (j = 0; j < N_K; j++)    // SUGRA
-    node0_printf("OS %d %d %d %.8g\n", Nsmear, block, j, OS[j] / norm);
-}
-// -----------------------------------------------------------------
+../../4d_Q16/susy/blocked_ops.c

commit 40986d119bb649e665859c29c70ac29dcf46e44d

Fix silly typos introduced by 27efc0e

4d_Q16/susy/hvy_pot_loop.c
@@ -19,7 +19,7 @@ void hvy_pot_loop(int do_det) {
   // Use tempmat to hold loop product at each site
   for (t_dist = 1; t_dist <= MAX_T; t_dist++) {
     // Set up rectangular path as list of dir * sign
-    FORALLUPDIR(mu) {
+    for (mu = XUP; mu <= ZUP; mu++) {
       for (x_dist[mu] = 0; x_dist[mu] <= MAX_X; x_dist[mu]++) {
         if (mu > 0 && x_dist[mu] == 0)
           x_dist[mu] = 1;   // Only check (0, T) "loop" once

4d_Q16/susy/hvy_pot_polar_loop.c
@@ -30,7 +30,7 @@ void hvy_pot_polar_loop() {
   // Use tempmat to hold loop product at each site
   for (t_dist = 1; t_dist <= MAX_T; t_dist++) {
     // Set up rectangular path as list of dir * sign
-    FORALLUPDIR(mu) {
+    for (mu = XUP; mu <= ZUP; mu++) {
       for (x_dist[mu] = 0; x_dist[mu] <= MAX_X; x_dist[mu]++) {
         if (mu > 0 && x_dist[mu] == 0)
           x_dist[mu] = 1;   // Only check (0, T) "loop" once