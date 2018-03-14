a95553b5f6965c75a15cf9a6d94d0de8e89cb751
Anosh Joseph <anoshjoseph@gmail.com>
2018-03-15 01:21:07 +0530

    Some work on the fermions.

--- 1d_Q16/susy/congrad_multi.c
+++ 1d_Q16/susy/congrad_multi.c
@@ -1,1 +1,266 @@
-../../4d_Q16/susy/congrad_multi.c
+// -----------------------------------------------------------------
+// Multi-mass conjugate gradient algorithm a la B. Jegerlehner
+// The number of masses is runtime input Norder
+// shift[Norder] is the array of mass values, set up in setup_rhmc.c
+// BEWARE: The temporary TF pm[0] is never malloced or used
+
+// At least for now we hard-code a zero initial guess
+// We check all psi for convergence and quit doing the converged ones
+
+// In this version of the code, all the scalars are real because M = Ddag D
+//#define CG_DEBUG
+#include "susy_includes.h"
+// -----------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------
+// Keep all Norder-dependent mallocs here so that we can change Norder
+// Return number of iterations
+// src is where the source is created
+// psim[Norder] are working TFs for the conjugate gradient
+// MaxCG is the maximum number of iterations per solve
+// errormin is the target |r|^2, scaled below by source_norm = |src|^2
+// size_r is the final obtained |r|^2, hopefully < errormin * source_norm
+#ifndef PUREGAUGE
+int congrad_multi(matrix *src[NFERMION], matrix **psim[NFERMION],
+                  int MaxCG, Real errormin, Real *size_r) {
+
+  register int i, j, k;
+  register site *s;
+  int N_iter, iteration = 0;
+  int *converged = malloc(sizeof *converged * Norder);
+  Real floatvar, floatvar2;     // SSE kluge
+  Real *floatvarj = malloc(sizeof *floatvarj * Norder);
+  Real *floatvark = malloc(sizeof *floatvark * Norder);
+  double rsq, rsqnew, source_norm = 0.0, rsqstop, c1, c2, cd;
+  double *zeta_i   = malloc(sizeof *zeta_i * Norder);
+  double *zeta_im1 = malloc(sizeof *zeta_im1 * Norder);
+  double *zeta_ip1 = malloc(sizeof *zeta_ip1 * Norder);
+  double *beta_i   = malloc(sizeof *beta_i * Norder);
+  double *beta_im1 = malloc(sizeof *beta_im1 * Norder);
+  double *alpha    = malloc(sizeof *alpha * Norder);
+  double rsqj;
+  complex ctmp;
+  matrix **pm[NFERMION];
+  
+  for (j = 0; j < NFERMION; j++) {
+    pm[j] = malloc(sizeof(matrix*) * Norder);
+    
+    for (i = 1; i < Norder; i++)    // !!!
+      pm[j][i] = malloc(sizeof(matrix) * sites_on_node);
+  }
+  
+  
+  // Initialize zero initial guess, etc.
+  // dest = 0, r = source, pm[j] = r
+  for (i = 0; i < Norder; i++)
+    converged[i] = 0;
+  for (k = 0; k < NFERMION; k++) {
+    FORALLSITES(i, s) {
+      mat_copy(&(src[k][i]), &(rm[k][i]));
+      mat_copy(&(rm[k][i]), &(pm0[k][i]));
+      clear_mat(&(psim[k][0][i]));
+      for (j = 1; j < Norder; j++) {
+        clear_mat(&(psim[k][j][i]));
+        mat_copy(&(rm[k][i]), &(pm[k][j][i]));
+      }
+    }
+  }
+  
+  for (k = 0; k < NFERMION; k++) {
+  FORALLSITES(i, s)
+    source_norm += (double)realtrace(&(src[k][i]), &(src[k][i]));
+  }
+  g_doublesum(&source_norm);
+  rsq = source_norm;
+  rsqstop = errormin * source_norm;
+#ifdef CG_DEBUG
+  node0_printf("congrad: source_norm = %.4g\n", source_norm);
+  node0_printf("stopping when residue is %.4g\n", rsqstop);
+#endif
+
+  for (j = 0; j < Norder; j++) {
+    zeta_im1[j] = 1;
+    zeta_i[j] = 1;
+    alpha[j] = 0;
+    beta_im1[j] = 1;
+  }
+
+  for (N_iter = 0; N_iter < MaxCG && rsq > rsqstop; N_iter++) {
+    // mp = (M(u) + shift[0]) pm
+    DSq(pm0, mpm);
+    iteration++;
+    total_iters++;
+    for (k = 0; k < NFERMION; k++) {
+    FORALLSITES(i, s)
+      scalar_mult_sum_matrix(&(pm0[k][i]), shift[0], &(mpm[k][i]));
+    }
+    // beta_i[0] = -(r, r) / (pm, Mpm)
+    cd = 0;
+    for (k = 0; k < NFERMION; k++) {
+      FORALLSITES(i, s) {
+        ctmp = complextrace_an(&(pm0[k][i]), &(mpm[k][i]));
+        cd += ctmp.real;
+      }
+    }
+    g_doublesum(&cd);
+
+    beta_i[0] = -rsq / cd;
+#ifdef CG_DEBUG
+    node0_printf("beta_i %.4g rsq %.4g cd %.4g\n", beta_i[0], rsq, cd);
+#endif
+
+    // beta_i(sigma)
+    // zeta_ip1(sigma)
+    zeta_ip1[0] = 1;
+    for (j = 1; j < Norder; j++) {
+      if (converged[j] == 0) {
+        zeta_ip1[j] = zeta_i[j] * zeta_im1[j] * beta_im1[0];
+        c1 = beta_i[0] * alpha[0] * (zeta_im1[j] - zeta_i[j]);
+        c2 = zeta_im1[j] * beta_im1[0] * (1 - (shift[j] - shift[0]) * beta_i[0]);
+        zeta_ip1[j] /= c1 + c2;
+        beta_i[j] = beta_i[0] * zeta_ip1[j] / zeta_i[j];
+      }
+    }
+
+    // psim[j] = psim[j] - beta[j] * pm[j]
+    floatvar = -(Real)beta_i[0];
+    for (j = 1; j < Norder; j++) {
+      if (converged[j] == 0)
+        floatvarj[j] = -(Real)beta_i[j];
+    }
+
+    for (k = 0; k < NFERMION; k++) {
+      FORALLSITES(i, s) {
+        scalar_mult_sum_matrix(&(pm0[k][i]), floatvar, &(psim[k][0][i]));
+        for (j = 1; j < Norder; j++) {
+          if (converged[j] == 0)
+            scalar_mult_sum_matrix(&(pm[k][j][i]), floatvarj[j], &(psim[k][j][i]));
+        }
+      }
+    }
+
+    // r = r + beta[0] * mp
+    floatvar = (Real)beta_i[0];
+    for (k = 0; k < NFERMION; k++) {
+      FORALLSITES(i, s)
+      scalar_mult_sum_matrix(&(mpm[k][i]), floatvar, &(rm[k][i]));
+    }
+    // alpha_ip1[j]
+    rsqnew = 0;
+    for (k = 0; k < NFERMION; k++) {
+    FORALLSITES(i, s)
+      rsqnew += (double)realtrace(&(rm[k][i]), &(rm[k][i]));
+    }
+    g_doublesum(&rsqnew);
+    alpha[0] = rsqnew / rsq;
+#ifdef CG_DEBUG
+    node0_printf("alpha %.4g rsqnew %.4g rsq %.4g\n", alpha[0], rsqnew, rsq);
+#endif
+
+    // alpha_ip11 -- note shifted indices with respect to Eq. 2.43!
+    for (j = 1; j < Norder; j++) {
+      if (converged[j] == 0)
+        alpha[j] = alpha[0] * zeta_ip1[j] * beta_i[j] / (zeta_i[j] * beta_i[0]);
+    }
+
+    // pm[j] = zeta_ip1[j] * r + alpha[j] * pm[j]
+    floatvar  = (Real)zeta_ip1[0];
+    floatvar2 = (Real)alpha[0];
+    for (j = 1; j < Norder; j++) {
+      floatvarj[j] = (Real)zeta_ip1[j];
+      floatvark[j] = (Real)alpha[j];
+    }
+    for (k = 0; k < NFERMION; k++) {
+      FORALLSITES(i, s) {
+        scalar_mult_matrix(&(rm[k][i]),floatvar, &(mpm[k][i]));
+        scalar_mult_add_matrix(&(mpm[k][i]), &(pm0[k][i]), floatvar2, &(pm0[k][i]));
+        for (j = 1; j < Norder; j++) {
+          if (converged[j] == 0) {
+            scalar_mult_matrix(&(rm[k][i]), floatvarj[j], &(mpm[k][i]));
+            scalar_mult_add_matrix(&(mpm[k][i]), &(pm[k][j][i]), floatvark[j], &(pm[k][j][i]));
+          }
+        }
+      }
+    }
+    // Test for convergence
+    rsq = rsqnew;
+    for (j = 1; j < Norder; j++) {
+      if (converged[j] == 0) {
+        rsqj = rsq * zeta_ip1[j] * zeta_ip1[j];
+        if (rsqj <= rsqstop) {
+          converged[j] = 1;
+#ifdef CG_DEBUG
+          node0_printf(" psi%d converged in %d steps, rsq = %.4g\n",
+                       j, N_iter, rsqj);
+#endif
+        }
+      }
+    }
+#ifdef CG_DEBUG
+    if ((N_iter / 10) * 10 == N_iter) {
+      node0_printf("iter %d residue %.4g\n", N_iter, (double)(rsq));
+      fflush(stdout);
+    }
+#endif
+
+    // Scroll scalars
+    for (j = 0; j < Norder; j++) {
+      if (converged[j] == 0) {
+        beta_im1[j] = beta_i[j];
+        zeta_im1[j] = zeta_i[j];
+        zeta_i[j] = zeta_ip1[j];
+      }
+    }
+  }
+  if (rsq > rsqstop)
+    node0_printf(" multi CONGRAD not converged\n rsq = %.4g\n", rsq);
+
+  *size_r = rsq;
+
+  // Test inversion
+#ifdef CG_DEBUG
+  for (j = 0; j < Norder; j++) {
+    source_norm = 0;
+    for (k = 0; k < NFERMION; k++) {
+    FORALLSITES(i, s)
+      source_norm += (double)realtrace(&(psim[k][j][i]), &(psim[k][j][i]));
+    }
+    g_doublesum(&source_norm);
+    node0_printf("Norm of psim %d shift %.4g is %.4g\n",
+                 j, shift[j], source_norm);
+
+    DSq(psim[j], mpm);              // mpm = (D^2 + fmass^2).psim[j]
+    source_norm = 0;                // Re-using for convenience
+    for (k = 0; k < NFERMION; k++) {
+      FORALLSITES(i, s) {             // Add shift.psi and subtract src
+        scalar_mult_sum_matrix(&(psim[k][j][i]), shift[j],  &(mpm[k][i]));
+        scalar_mult_sum_matrix(&(src[k][i]), -1.0, &(mpm[k][i]));
+        source_norm += (double)realtrace(&(mpm[k][i]), &(mpm[k][i]));
+      }
+    }
+    g_doublesum(&source_norm);
+    node0_printf("%d Test of (D^2 + shift)psi - src: %.4g\n", j, source_norm);
+  }
+#endif
+
+  for (k = 0; k < NFERMION; k++) {
+    for (i = 1; i < Norder; i++)
+      free(pm[k][i]);
+    free(pm[k]);
+  }
+
+  free(zeta_i);
+  free(zeta_ip1);
+  free(zeta_im1);
+  free(beta_im1);
+  free(beta_i);
+  free(alpha);
+  free(converged);
+  free(floatvarj);
+  free(floatvark);
+  return iteration;
+}
+#endif
+// -----------------------------------------------------------------
--- 1d_Q16/susy/utilities.c
+++ 1d_Q16/susy/utilities.c
@@ -4,143 +4,35 @@
 // -----------------------------------------------------------------
 
 
-
-// -----------------------------------------------------------------
-// Term in action connecting site fermion to the link fermions
-// bc[mu](x) on psi_mu(x) eta(x + mu)
-// Add to dest instead of overwriting; note factor of 1/2
-#ifdef SV
-void DbplusStoL(matrix *src, matrix *dest[NUMLINK]) {
-  register int i;
-  register site *s;
-  int mu;
-  msg_tag *tag[NUMLINK];
-  matrix tmat;
-
-  tag[0] = start_gather_field(src, sizeof(matrix), goffset[0],
-                              EVENANDODD, gen_pt[0]);
-  FORALLDIR(mu) {
-    if (mu == 0)     // Start other gather
-      tag[1] = start_gather_field(src, sizeof(matrix), goffset[1],
-                                  EVENANDODD, gen_pt[1]);
-
-    wait_gather(tag[mu]);
-    FORALLSITES(i, s) {
-      mult_na((matrix *)(gen_pt[mu][i]), &(s->link[mu]), &tmat);
-      scalar_mult_matrix(&tmat, s->bc[mu], &tmat);
-      mult_an_dif(&(s->link[mu]), &(src[i]), &tmat);
-      scalar_mult_sum_matrix(&tmat, 0.5, &(dest[mu][i]));
-    }
-    cleanup_gather(tag[mu]);
-  }
-}
-#endif
-// -----------------------------------------------------------------
-
-
-
-// -----------------------------------------------------------------
-// Term in action connecting the link fermions to the site fermion
-// Given src psi_a, dest is Dbar_a psi_a (Eq. 63 in the arXiv:1108.1503)
-// Use tempmat and tempmat2 for temporary storage
-// bc[OPP_LDIR(mu)](x) on eta(x - mu) psi_mu(x - mu)
-// Initialize dest; note factor of 1/2
-#ifdef SV
-void DbminusLtoS(matrix *src[NUMLINK], matrix *dest) {
-  register int i, mu, opp_mu;
-  register site *s;
-  msg_tag *tag[NUMLINK];
-
-  FORALLSITES(i, s) {         // Set up first gather
-    clear_mat(&(dest[i]));     // Initialize
-    mult_an(&(s->link[0]), &(src[0][i]), &(tempmat[i]));
-  }
-  tag[0] = start_gather_field(tempmat, sizeof(matrix),
-                              goffset[0] + 1, EVENANDODD, gen_pt[0]);
-
-  FORALLDIR(mu) {
-    if (mu == 0) {                  // Start other gather
-      FORALLSITES(i, s)
-        mult_an(&(s->link[1]), &(src[1][i]), &(tempmat2[i]));
-      tag[1] = start_gather_field(tempmat2, sizeof(matrix),
-                                  goffset[1] + 1, EVENANDODD, gen_pt[1]);
-    }
-
-    opp_mu = OPP_LDIR(mu);
-    wait_gather(tag[mu]);
-    FORALLSITES(i, s) {
-      scalar_mult_dif_matrix((matrix *)(gen_pt[mu][i]), s->bc[opp_mu],
-                             &(dest[i]));
-      mult_na_sum(&(src[mu][i]), &(s->link[mu]), &(dest[i]));
-    }
-    cleanup_gather(tag[mu]);
-  }
-
-  // Overall factor of 1/2
-  FORALLSITES(i, s)
-    scalar_mult_matrix(&(dest[i]), 0.5, &(dest[i]));
-}
-#endif
-// -----------------------------------------------------------------
-
-
 // -----------------------------------------------------------------
 // Matrix--vector operation
 // Applies either the operator (sign = 1) or its adjoint (sign = -1)
 #ifndef PUREGAUGE
 void fermion_op(matrix *src[NFERMION], matrix *dest[NFERMION], int sign) {
-  register int i, mu;
+  register int i, j;
   register site *s;
-
-  // Copy src matrix into fieldwise site, link and plaq fermions,
-  // overwriting all of the latter
-  if (sign == 1) {
+  matrix tmat;
+  
+  if (sign == -1) {
     FORALLSITES(i, s) {
-      mat_copy(&(src[i].Fsite), &(site_src[i]));
-      FORALLDIR(mu)
-        mat_copy(&(src[i].Flink[mu]), &(link_src[mu][i]));
-      mat_copy(&(src[i].Fplaq), &(plaq_src[i]));
+      for(j=0;j<NFERMION;j++)
+      {
+        adjoint(&(src[j][i]), &tmat);
+        mat_copy(&tmat, &(src[j][i]));
+      }
     }
   }
-  else if (sign == -1) {
-    FORALLSITES(i, s) {
-      adjoint(&(src[i].Fsite), &(site_src[i]));
-      FORALLDIR(mu)
-        adjoint(&(src[i].Flink[mu]), &(link_src[mu][i]));
-      adjoint(&(src[i].Fplaq), &(plaq_src[i]));
-    }
-  }
-  else {
-    node0_printf("Error: incorrect sign in fermion_op: %d\n", sign);
-    terminate(1);
-  }
-  FORALLSITES(i, s)
-    tr_eta[i] = trace(&(site_src[i]));
+  
+  // TODO: Fermion operator goes here
 
-  // Assemble separate routines for each term in the fermion operator
-#ifdef SV
-  DbplusStoL(site_src, link_dest);        // Adds to link_dest
-
-  // Site-to-link plaquette determinant contribution if G is non-zero
-  // Only depends on Tr[eta(x)]
-  DbminusLtoS(link_src, site_dest);       // Overwrites site_dest
-#endif
-
-  // Copy local plaquette, link and site fermions into dest TwistFermion
-  if (sign == 1) {
+  
+  if (sign == -1) {    // Both negate and conjugate
     FORALLSITES(i, s) {
-      mat_copy(&(site_dest[i]), &(dest[i].Fsite));
-      FORALLDIR(mu)
-        mat_copy(&(link_dest[mu][i]), &(dest[i].Flink[mu]));
-      mat_copy(&(plaq_dest[i]), &(dest[i].Fplaq));
-    }
-  }
-  else if (sign == -1) {    // Both negate and conjugate
-    FORALLSITES(i, s) {
-      neg_adjoint(&(site_dest[i]), &(dest[i].Fsite));
-      FORALLDIR(mu)
-        neg_adjoint(&(link_dest[mu][i]), &(dest[i].Flink[mu]));
-      neg_adjoint(&(plaq_dest[i]), &(dest[i].Fplaq));
+      for(j=0;j<NFERMION;j++)
+      {
+        adjoint(&(dest[j][i]), &tmat);
+        scalar_mult_matrix(&tmat, -1.0 , &(dest[j][i]));
+      }
     }
   }
 }
