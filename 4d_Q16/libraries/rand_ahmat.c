38af977f00f8444064ce222ed3eea3e8d3ef3e7f
daschaich <daschaich@gmail.com>
2018-04-29 00:25:18 +0200

    Fix rand_ahmat, but N=7 test is still problematic

--- 4d_Q16/libraries/rand_ahmat.c
+++ 4d_Q16/libraries/rand_ahmat.c
@@ -7,14 +7,19 @@
 #include "../include/complex.h"
 #include "../include/susy.h"
 
+#ifdef RANDAH_DEBUG
+#include <stdio.h>
+#endif
+
 void random_anti_hermitian(anti_hermitmat *ahmat, double_prn *prn_pt) {
   Real r3;
 #if (NCOL > 2)
-  Real r8, one_ov_root3;
+  Real r8;
 #if (NCOL > 3)
-  Real r15, sqrt_sixth;
+  Real r15;
 #if (NCOL > 4)
-  int i;
+  int i, j;
+  Real tr;
 #endif
 #endif
 #endif
@@ -65,12 +70,31 @@ void random_anti_hermitian(anti_hermitmat *ahmat, double_prn *prn_pt) {
   ahmat->im_diag[3] =      -3.0 * r15;
 #endif
 #if (NCOL > 4)
-  ahmat->im_diag[3] *= -1.0;
-  for (i = 4; i < NCOL - 1; i++)
-    ahmat->im_diag[i] = 3.0 * r15;
+  for (i = 4; i < NCOL; i++) {
+    // Reuse r15 for next properly scaled random number
+    // Scaling factor is sqrt(2 / (i * (i + 1)))
+    // Check: Previously sqrt of 1, 1/3, 1/6 for i=1, 2, 3
+    r15 = gaussian_rand_no(prn_pt);
+    tr = (Real)i * (i + 1);
+    r15 *= sqrt((double)(1.0 / tr));
 
-  // For tracelessness, the factor should be (NCOL - 3)
-  ahmat->im_diag[NCOL - 1] = -3.0 * (NCOL - 4) * r15;
+    // Add new r15 to i existing elements
+    for (j = 0; j < i; j++)
+      ahmat->im_diag[j] += r15;
+
+    // Initialize next element with -i * r15
+    ahmat->im_diag[i] = -1.0 * i * r15;
+  }
+#endif
+
+#ifdef RANDAH_DEBUG
+  // Check tracelessness
+  int k;
+  r3 = ahmat->im_diag[0];
+  for (k = 1; k < NCOL; k++)
+    r3 += ahmat->im_diag[k];
+  if (fabs(r3) > 1e-6)
+    printf("Warning: random_anti_hermitian trace = %.4g\n", r3);
 #endif
 }
 // -----------------------------------------------------------------
