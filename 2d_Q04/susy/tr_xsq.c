9647c24ba21ef0d37bb5dbd93f94272b635eabff
Anosh Joseph <30601557+anoshjoseph@users.noreply.github.com>
2021-01-08 22:17:34 +0530

    Add overlooked file

---
+++ 2d_Q04/susy/tr_xsq.c
@@ -0,0 +1,33 @@
+// -----------------------------------------------------------------
+// Measure Tr X^2 where X is the scalar field from
+// the polar decomposition
+#include "susy_includes.h"
+
+void measure_tr_xsq() {
+  register int i, dir;
+  register site *s;
+  double norm = 1.0 / ((double)(NUMLINK * volume * NCOL));
+  complex tc, tr_xsq = cmplx(0.0, 0.0);
+  matrix X, tmat;
+  
+  FORALLSITES(i, s) {
+    FORALLDIR(dir) {
+      polar(&(s->link[dir]), &X, &tmat);
+      // Take log
+      matrix_log(&tmat, &X);
+      
+      tc = complextrace_nn(&X, &X);
+      CSUM(tr_xsq, tc);
+    }
+  }
+  
+  CMULREAL(tr_xsq, norm, tr_xsq);
+  
+  g_complexsum(&tr_xsq);
+  
+  if (fabs(tr_xsq.imag) > IMAG_TOL)
+    node0_printf("WARNING Im(tr_xsq) = %.4g\n", tr_xsq.imag);
+    
+  node0_printf("TR_XSQ %.8g\n", tr_xsq.real);
+}
+// -----------------------------------------------------------------
