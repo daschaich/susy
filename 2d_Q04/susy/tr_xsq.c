5e92ae5853b8b5aa9c2e93441afed543a973006c
daschaich <daschaich@gmail.com>
2021-04-12 18:55:31 +0100

    Quick clarifications and cleanup

--- 2d_Q04/susy/tr_xsq.c
+++ 2d_Q04/susy/tr_xsq.c
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------
 // Measure Tr[X^2] / N, where X is the scalar field
-// from the polar decomposition
+// from the log of hermitian part of the polar decomposition
 #include "susy_includes.h"
 
 void measure_tr_xsq() {
@@ -9,23 +9,24 @@ void measure_tr_xsq() {
   double norm = 1.0 / ((double)(NUMLINK * volume * NCOL));
   complex tc, tr_xsq = cmplx(0.0, 0.0);
   matrix X, tmat;
-  
+
   FORALLSITES(i, s) {
     FORALLDIR(dir) {
       polar(&(s->link[dir]), &X, &tmat);
-      // Take log
       matrix_log(&tmat, &X);
-      
+
+      // Note that we sum over both links
+      // and don't normalize by 2 below...
       tc = complextrace_nn(&X, &X);
       CSUM(tr_xsq, tc);
     }
   }
   CMULREAL(tr_xsq, norm, tr_xsq);
   g_complexsum(&tr_xsq);
-  
+
   if (fabs(tr_xsq.imag) > IMAG_TOL)
     node0_printf("WARNING Im(tr_xsq) = %.4g\n", tr_xsq.imag);
-    
+
   node0_printf("TR_XSQ %.8g\n", tr_xsq.real);
 }
 // -----------------------------------------------------------------
